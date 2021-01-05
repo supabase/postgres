64ae60cf3cb21d4f698869d3962343575b10fe67
Angelico <angelico.delosreyes@gmail.com>
2021-01-05 17:40:27 +0800

    fix: add pgaudit for docker shared_preload_libs - was somehow removed before

--- docker/mnt/init-permissions.sh
+++ docker/mnt/init-permissions.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "host replication $POSTGRES_USER 0.0.0.0/0 trust" >> $PGDATA/pg_hba.conf
-echo "shared_preload_libraries = 'pg_stat_statements, plpgsql, plpgsql_check, pg_cron'" >> $PGDATA/postgresql.conf
+echo "shared_preload_libraries = 'pg_stat_statements, pgaudit, plpgsql, plpgsql_check, pg_cron'" >> $PGDATA/postgresql.conf
 echo "pg_stat_statements.max = 10000" >> $PGDATA/postgresql.conf
 echo "pg_stat_statements.track = all" >> $PGDATA/postgresql.conf
 echo "wal_level=logical" >> $PGDATA/postgresql.conf
