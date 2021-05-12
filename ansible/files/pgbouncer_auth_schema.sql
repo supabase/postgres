d26bec8d840e363f21fa9fe3c1acbe33f2363787
Div Arora <darora@users.noreply.github.com>
2021-05-12 13:01:05 +0800

    Add postgres exporter (#41)

--- ansible/files/pgbouncer_auth_schema.sql
+++ ansible/files/pgbouncer_auth_schema.sql
@@ -1,13 +1,13 @@
 CREATE USER pgbouncer;
 
 CREATE SCHEMA pgbouncer AUTHORIZATION pgbouncer;
- 
+
 CREATE OR REPLACE FUNCTION pgbouncer.get_auth(p_usename TEXT)
 RETURNS TABLE(username TEXT, password TEXT) AS
 $$
 BEGIN
     RAISE WARNING 'PgBouncer auth request: %', p_usename;
- 
+
     RETURN QUERY
     SELECT usename::TEXT, passwd::TEXT FROM pg_catalog.pg_shadow
     WHERE usename = p_usename;
