0d284a54f5e7783b9ce53c5cc777657ca1dabaef
dragarcia <angelico.delosreyes@gmail.com>
2022-03-23 12:19:07 +0100

    removed unused scripts

--- ansible/files/admin_api_scripts/disable_walg_restore.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-
-sed -i "s/restore_command/#restore_command/" /etc/postgresql/postgresql.conf
-sed -i "s/recovery_target_time/#recovery_target_time/" /etc/postgresql/postgresql.conf
-sed -i "s/recovery_target_action/#recovery_target_action/" /etc/postgresql/postgresql.conf
-
-echo "WAL-G restoration disabled for future restarts"
