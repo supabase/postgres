a7db18e6d3efe9acc4ef645377b3010367e916f6
dragarcia <angelico.delosreyes@gmail.com>
2022-03-23 11:35:42 +0100

    Run backup in background

--- ansible/files/admin_api_scripts/commence_walg_backup.sh
+++ ansible/files/admin_api_scripts/commence_walg_backup.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-wal-g backup-push /var/lib/postgresql/data --config /etc/wal-g/config.json --verify >> /var/log/wal-g/backup-push 2>&1
+wal-g backup-push /var/lib/postgresql/data --config /etc/wal-g/config.json --verify >> /var/log/wal-g/backup-push 2>&1 &
 
 echo "WAL-G backup complete"
