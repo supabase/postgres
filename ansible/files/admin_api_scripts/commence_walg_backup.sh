cb94a4100ceb7fbca53350e9cad3b26f12a8d9e0
dragarcia <angelico.delosreyes@gmail.com>
2022-03-24 13:58:19 +0100

    ensure that it's config.json, not .env

--- ansible/files/admin_api_scripts/commence_walg_backup.sh
+++ ansible/files/admin_api_scripts/commence_walg_backup.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-WALG_SENTINEL_USER_DATA="{ \"backup_id\": $1, \"project_id\": $2 }" nohup wal-g backup-push /var/lib/postgresql/data --config /etc/wal-g/config.env --verify >> /var/log/wal-g/backup-push.log 2>&1 &
+WALG_SENTINEL_USER_DATA="{ \"backup_id\": $1, \"project_id\": $2 }" nohup wal-g backup-push /var/lib/postgresql/data --config /etc/wal-g/config.json --verify >> /var/log/wal-g/backup-push.log 2>&1 &
 
 echo "WAL-G backup job commenced"
