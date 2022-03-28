cc1d0e8d978b5dbbc8e5c15761f1c66d50045b89
dragarcia <angelico.delosreyes@gmail.com>
2022-03-28 12:34:32 +0200

    script to complete wal-g restoration

---
+++ ansible/files/admin_api_scripts/complete_walg_restore.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+# disable 169.254.169.254 for postgres
+sed -i "/#\\sdon't\\sdelete\\sthe\\s'COMMIT'/ i -I OUTPUT 1 --proto tcp --destination 169.254.169.254 --match owner --uid-owner postgres --jump REJECT\\n" /etc/ufw/before.rules
+ufw reload
+
+# move config file to its final location and change its ownership
+mv /etc/postgresql/wal-g-config.json /etc/wal-g/config.json
+chown wal-g:wal-g /etc/wal-g/config.json
+
+
+# disable recovery commands in the event of a restart
+sed -i "s/.*restore_command/#restore_command/" /etc/postgresql/postgresql.conf
+sed -i "s/.*recovery_target_time/#recovery_target_time/" /etc/postgresql/postgresql.conf
+sed -i "s/.*recovery_target_action/#recovery_target_action/" /etc/postgresql/postgresql.conf
+
+# enable archive_command
+sed -i "s/.*archive_mode/archive_mode/" /etc/postgresql/postgresql.conf
+sed -i "s/.*archive_command/archive_command/" /etc/postgresql/postgresql.conf
+sed -i "s/.*archive_timeout/archive_timeout/" /etc/postgresql/postgresql.conf
+
+systemctl restart postgresql
+
+echo "Cleanup post WAL-G restoration complete"
