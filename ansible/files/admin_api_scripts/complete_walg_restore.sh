96c7b3403dd71ff13c26232cb921b28ef7b8b766
dragarcia <angelico.delosreyes@gmail.com>
2022-03-31 13:45:05 +0200

    modify wal-g related parameters in separate file

--- ansible/files/admin_api_scripts/complete_walg_restore.sh
+++ ansible/files/admin_api_scripts/complete_walg_restore.sh
@@ -12,14 +12,14 @@ chown wal-g:wal-g /etc/wal-g/config.json
 
 
 # disable recovery commands in the event of a restart
-sed -i "s/.*restore_command/#restore_command/" /etc/postgresql/postgresql.conf
-sed -i "s/.*recovery_target_time/#recovery_target_time/" /etc/postgresql/postgresql.conf
-sed -i "s/.*recovery_target_action/#recovery_target_action/" /etc/postgresql/postgresql.conf
+sed -i "s/.*restore_command/#restore_command/" /etc/postgresql-custom/wal-g.conf
+sed -i "s/.*recovery_target_time/#recovery_target_time/" /etc/postgresql-custom/wal-g.conf
+sed -i "s/.*recovery_target_action/#recovery_target_action/" /etc/postgresql-custom/wal-g.conf
 
 # enable archive_command
-sed -i "s/.*archive_mode/archive_mode/" /etc/postgresql/postgresql.conf
-sed -i "s/.*archive_command/archive_command/" /etc/postgresql/postgresql.conf
-sed -i "s/.*archive_timeout/archive_timeout/" /etc/postgresql/postgresql.conf
+sed -i "s/.*archive_mode/archive_mode/" /etc/postgresql-custom/wal-g.conf
+sed -i "s/.*archive_command/archive_command/" /etc/postgresql-custom/wal-g.conf
+sed -i "s/.*archive_timeout/archive_timeout/" /etc/postgresql-custom/wal-g.conf
 
 systemctl restart postgresql
 
--- ansible/files/admin_api_scripts/enable_walg.sh
+++ ansible/files/admin_api_scripts/enable_walg.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-sed -i "s/.*archive_mode/archive_mode/" /etc/postgresql/postgresql.conf
-sed -i "s/.*archive_command/archive_command/" /etc/postgresql/postgresql.conf
-sed -i "s/.*archive_timeout/archive_timeout/" /etc/postgresql/postgresql.conf
+sed -i "s/.*archive_mode/archive_mode/" /etc/postgresql-custom/wal-g.conf
+sed -i "s/.*archive_command/archive_command/" /etc/postgresql-custom/wal-g.conf
+sed -i "s/.*archive_timeout/archive_timeout/" /etc/postgresql-custom/wal-g.conf
 
 systemctl restart postgresql
 
