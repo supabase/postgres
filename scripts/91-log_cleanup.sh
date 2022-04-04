adbae713c60c88330674b812ec9255ec7e6e0515
dragarcia <Angelico.delosreyes@gmail.com>
2022-04-05 01:57:57 +0800

    Implement configurations for WAL-G (#145)
    * chore: create wal-g Linux user
    - Let postgres Linux user execute wal-g as wal-g Linux user
    
    * Revert WAL-G backport
    
    * chore: give read access to PG data directory to groups
    - specifically to groups that the postgres Linux user are a part of
    
    * update configs in postgresql.conf
    - not enabled however
    
    * WAL-G specific scripts to be triggered via the admin API
    
    * Create config directory for wal-g
    
    * chore: add logging for wal-g processes
    
    * ensure bindir when installing pgsodium works for EC2 builds
    
    * triggere dockerbuild
    
    * Run backup in background
    
    * Inject WALG_SENTINEL_USER_DATA during backups
    - Also, use nohup
    
    * Add scripts for enabling and disabling walg
    
    * removed unused scripts
    
    * bump versioning
    
    * cleanup
    
    * Move logrotate to its own dir
    - to allow looping through
    
    * cleanup: loop through creation of log files
    
    * set up logrotate for all WAL-G log files
    
    * Ensure backup is pointing to the correct log file
    
    * create postgres linux group
    
    * ensure that it's config.json, not .env
    
    * allow admin API to edit /etc/wal-g/config.json
    
    * add whitespace
    
    * script to complete wal-g restoration
    
    * update adminapi
    
    * remove bindir in setup-system
    
    * create wal-g group and move up setting up of wal-g
    
    * create wal-g log files towards the end
    
    * Ensure /var/lib/postgresql/data is owned by postgres:postgres
    
    * fix transfer of file found in subdirectories
    
    * set primary groups for postgres and wal-g linux users
    
    * newline
    
    * modify wal-g related parameters in separate file
    
    * bump to 14.1.0.21
    
    * more newlines

--- scripts/91-log_cleanup.sh
+++ scripts/91-log_cleanup.sh
@@ -12,3 +12,9 @@ chown pgbouncer:postgres /var/log/pgbouncer.log
 
 mkdir /var/log/postgresql
 chown postgres:postgres /var/log/postgresql
+
+mkdir /var/log/wal-g
+cd /var/log/wal-g
+touch backup-push.log backup-fetch.log wal-push.log wal-fetch.log
+chown -R wal-g:wal-g /var/log/wal-g
+chmod -R 0700 /var/log/wal-g
