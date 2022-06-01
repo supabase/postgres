e4749fa2fcfb2fbaedf4e02e3b023b62c4b48166
dragarcia <Angelico.delosreyes@gmail.com>
2022-06-01 18:27:20 +0800

    Changes for PITR process (#210)
    * additional default files pertaining to PITR
    
    * configuration changes to ensure new files work
    
    * limit chown to only files found in /tmp/*
    
    * switch between %f and %p
    
    * scripts for restore_command
    
    * put correct filename for restore_command
    
    * ensure restore_command scripts are included
    
    * add recovery_target_timeline
    
    * update restore command to be able to be run in bg
    
    * update cleanup script following a restoration
    
    * bump adminapi to 0.23.0
    
    * bump supabase/postgres version
    
    * cleanup
    
    * newline
    
    * ensure recovery_target_timeline is cleaned up as well
    
    * interchange injected values during backup
    
    * udpate wal_change_ownership.sh
    
    * udpate scripts
    * run shellcheck
    * standardise to [[ ]] for comparison

--- ansible/files/admin_api_scripts/complete_walg_restore.sh
+++ ansible/files/admin_api_scripts/complete_walg_restore.sh
@@ -2,25 +2,13 @@
 
 set -euo pipefail
 
-# disable 169.254.169.254 for postgres
-sed -i "/#\\sdon't\\sdelete\\sthe\\s'COMMIT'/ i -I OUTPUT 1 --proto tcp --destination 169.254.169.254 --match owner --uid-owner postgres --jump REJECT\\n" /etc/ufw/before.rules
-ufw reload
-
-# move config file to its final location and change its ownership
-mv /etc/postgresql/wal-g-config.json /etc/wal-g/config.json
-chown wal-g:wal-g /etc/wal-g/config.json
-
-
-# disable recovery commands in the event of a restart
+# Disable recovery commands in the event of a restart
 sed -i "s/.*restore_command/#restore_command/" /etc/postgresql-custom/wal-g.conf
 sed -i "s/.*recovery_target_time/#recovery_target_time/" /etc/postgresql-custom/wal-g.conf
 sed -i "s/.*recovery_target_action/#recovery_target_action/" /etc/postgresql-custom/wal-g.conf
+sed -i "s/.*recovery_target_timeline/#recovery_target_timeline/" /etc/postgresql-custom/wal-g.conf
 
-# enable archive_command
-sed -i "s/.*archive_mode/archive_mode/" /etc/postgresql-custom/wal-g.conf
-sed -i "s/.*archive_command/archive_command/" /etc/postgresql-custom/wal-g.conf
-sed -i "s/.*archive_timeout/archive_timeout/" /etc/postgresql-custom/wal-g.conf
-
-systemctl restart postgresql
+# Cleanup /tmp
+rm -rf /tmp/walg_data/ /tmp/.wal-g/ /tmp/wal_fetch_dir/
 
 echo "Cleanup post WAL-G restoration complete"
--- common.vars.pkr.hcl
+++ common.vars.pkr.hcl
@@ -1,1 +1,1 @@
-postgres-version = "14.1.0.33"
+postgres-version = "14.1.0.34"
