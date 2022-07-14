21407f5fafce9e7fd3aba25acee4c6679ac3ae31
Paul Cioanca <paul.cioanca@supabase.io>
2022-07-14 06:02:01 +0300

    feat: add support to allow expanding multiple volumes

--- ansible/files/admin_api_scripts/grow_fs.sh
+++ ansible/files/admin_api_scripts/grow_fs.sh
@@ -2,6 +2,22 @@
 
 set -euo pipefail
 
-resize2fs /dev/nvme1n1
+VOLUME_TYPE=${1:-data}
 
+if [ -b /dev/nvme1n1 ] ; then
+    if [[ "${VOLUME_TYPE}" == "data" ]]; then
+        resize2fs /dev/nvme1n1
+
+    elif [[ "${VOLUME_TYPE}" == "root" ]] ; then
+        growpart /dev/nvme0n1 2
+        resize2fs /dev/nvme0n1p2
+
+    else
+        echo "Invalid disk specified: ${VOLUME_TYPE}"
+        exit 1
+    fi
+else
+    growpart /dev/nvme0n1 2
+    resize2fs /dev/nvme0n1p2
+fi
 echo "Done resizing disk"
