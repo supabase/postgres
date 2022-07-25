79d63bfd68fce42af4e93b7892bc7ad432d5c718
Paul Cioanca <paul.cioanca@supabase.io>
2022-07-25 07:32:13 +0300

    feat: add separate EBS data volume at AMI build time (#244)
    * feat: add support to allow expanding multiple volumes
    
    * fix: edit node_exporter collection exclusion rules to include /data as a monitored mount path
    
    * feat: provision postgresql AMI with data volume included
    
    * chore: bump adminapi version

--- ebssurrogate/scripts/surrogate-bootstrap.sh
+++ ebssurrogate/scripts/surrogate-bootstrap.sh
@@ -110,6 +110,10 @@ function format_and_mount_rootfs {
 		sleep 2
 		mount /dev/xvdf1 /mnt/boot/efi
 	fi
+	
+	mkfs.ext4 /dev/xvdh
+	mkdir -p /mnt/data
+	mount -o defaults,discard /dev/xvdh /mnt/data
 }
 
 function format_build_partition {
@@ -127,6 +131,7 @@ cat > "/mnt/etc/fstab" << EOF
 $(printf "${FMT}" "# DEVICE UUID" "MOUNTPOINT" "TYPE" "OPTIONS" "DUMP" "FSCK")
 $(findmnt -no SOURCE /mnt | xargs blkid -o export | awk -v FMT="${FMT}" '/^UUID=/ { printf(FMT, $0, "/", "ext4", "defaults,discard", "0", "1" ) }')
 $(findmnt -no SOURCE /mnt/boot/efi | xargs blkid -o export | awk -v FMT="${FMT}" '/^UUID=/ { printf(FMT, $0, "/boot/efi", "vfat", "umask=0077", "0", "1" ) }')
+$(findmnt -no SOURCE /mnt/data | xargs blkid -o export | awk -v FMT="${FMT}" '/^UUID=/ { printf(FMT, $0, "/data", "ext4", "defaults,discard", "0", "2" ) }')
 EOF
 	unset FMT
 }
@@ -249,7 +254,8 @@ function umount_reset_mappings {
 	if [ "${ARCH}" = "arm64" ]; then
 		umount /mnt/boot/efi
 	fi
-        umount /mnt
+	umount /mnt/data
+	umount /mnt
 
 	# Reset device mappings
 	for dev_link in "${blkdev_mappings[@]}" "${partdev_mappings[@]}"; do
