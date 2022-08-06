db2aaf7ee6035660e3f5d1dae8a62f48ffe64070
Div Arora <root@darora.com>
2022-08-06 08:45:52 +0800

    fix: start postgresql post-resize if not running

--- ansible/files/admin_api_scripts/grow_fs.sh
+++ ansible/files/admin_api_scripts/grow_fs.sh
@@ -20,4 +20,9 @@ else
     growpart /dev/nvme0n1 2
     resize2fs /dev/nvme0n1p2
 fi
+
+if ! systemctl is-active postgresql --quiet ; then
+    systemctl start postgresql
+fi
+
 echo "Done resizing disk"
