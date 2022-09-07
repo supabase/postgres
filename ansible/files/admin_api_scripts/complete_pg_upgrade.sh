f82202baf87b5271649f7993275f169c2d885df7
Paul Cioanca <paul.cioanca@supabase.io>
2022-09-08 00:34:17 +0300

    chore: refactoring scripts; removing bin/lib copies

--- ansible/files/admin_api_scripts/complete_pg_upgrade.sh
+++ ansible/files/admin_api_scripts/complete_pg_upgrade.sh
@@ -1,11 +1,17 @@
 #! /usr/bin/env bash
 
+function complete_pg_upgrade {
+    mount -a -v
+
+    # copying custom configurations
+    cp /data/conf/* /etc/postgresql-custom/
+
+    service postgresql start
+    su -c 'vacuumdb --all --analyze-in-stages' -s $SHELL postgres
+    su -c 'psql -c "CREATE EXTENSION IF NOT EXISTS pg_graphql;"' -s $SHELL postgres
+}
+
 set -euo pipefail
 
-mount -a -v
-
-# copying custom configurations
-cp /data/conf/* /etc/postgresql-custom/
-
-service postgresql start
-vacuumdb --all --analyze-in-stages
+complete_pg_upgrade >> /var/log/pg-upgrade-complete.log 2>&1
+echo "Upgrade job completed"
--- ansible/files/admin_api_scripts/initiate_pg_upgrade.sh
+++ ansible/files/admin_api_scripts/initiate_pg_upgrade.sh
@@ -2,7 +2,8 @@
 
 set -euo pipefail
 
-PGPASSWORD=$1
+PGVERSION=$1
+PGPASSWORD=$2
 
 MOUNT_POINT="/data_migration"
 
@@ -23,46 +24,55 @@ cleanup() {
     exit $EXIT_CODE
 }
 
-if [ ! -b /dev/nvme2n1 ] ; then
-    echo "The block device at /dev/nvme2n1 is not mounted to system; exiting."
-    exit 1
-fi
+function initiate_upgrade {
+    BLOCK_DEVICE=$(lsblk -dpno name | grep -v "/dev/nvme[0-1]")
+
+    mkdir -p $MOUNT_POINT
+    mount $BLOCK_DEVICE $MOUNT_POINT
+
+    tar zxvf "$MOUNT_POINT/binaries/$PGVERSION.tar.gz" -C "$MOUNT_POINT/binaries"
+    chown -R postgres:postgres "$MOUNT_POINT/binaries/$PGVERSION"
+
+    run_sql "DROP EXTENSION IF EXISTS pg_graphql;"
+    run_sql "ALTER USER postgres WITH SUPERUSER;"
+
+    PGDATAOLD=$(cat /etc/postgresql/postgresql.conf | grep data_directory | sed "s/data_directory = '\(.*\)'.*/\1/");
+    PGDATANEW="$MOUNT_POINT/pgdata"
+    PGBINNEW="$MOUNT_POINT/binaries/$PGVERSION/bin"
+
+
+    rm -rf $PGDATANEW/*
+    su -c "$PGBINNEW/initdb -D $PGDATANEW/" -s $SHELL postgres
+
+    # running upgrade using at least 1 cpu core
+    WORKERS=$(nproc | awk '{ print ($1 == 1 ? 1 : $1 - 1) }')
+
+    # upgrade job outputs a log in the cwd; needs write permissions
+    cd /tmp
+
+    UPGRADE_COMMAND=$(cat <<EOF
+    time ${PGBINNEW}/pg_upgrade \
+    --old-bindir="/usr/lib/postgresql/bin" \
+    --new-bindir=${PGBINNEW} \
+    --old-datadir=${PGDATAOLD} \
+    --new-datadir=${PGDATANEW} \
+    --jobs="${WORKERS}" \
+    --old-options='-c config_file=/etc/postgresql/postgresql.conf' \
+    --new-options="-c data_directory=${PGDATANEW}"
+EOF
+    )
+
+    systemctl stop postgresql
+    su -c "$UPGRADE_COMMAND" -s $SHELL postgres
+
+    # copying custom configurations
+    mkdir -p $MOUNT_POINT/conf
+    cp /etc/postgresql-custom/* $MOUNT_POINT/conf/
+
+    cleanup
+}
 
 trap cleanup ERR
 
-mkdir -p $MOUNT_POINT
-mount /dev/nvme2n1 $MOUNT_POINT
-
-run_sql "DROP EXTENSION IF EXISTS pg_graphql;"
-run_sql "ALTER USER postgres WITH SUPERUSER;"
-
-rm -rf $MOUNT_POINT/pgdata/*
-su -c "/data/bin/initdb -D $MOUNT_POINT/pgdata/" -s $SHELL postgres
-
-PGDATAOLD=$(cat /etc/postgresql/postgresql.conf | grep data_directory | sed "s/data_directory = '\(.*\)'.*/\1/");
-PGDATANEW="$MOUNT_POINT/pgdata"
-PGBINNEW="$MOUNT_POINT/bin"
-
-# running upgrade using at least 1 cpu core
-WORKERS=$(nproc | awk '{ print ($1 == 1 ? 1 : $1 - 1) }')
-
-UPGRADE_COMMAND=$(cat <<EOF
-time ${PGBINNEW}/pg_upgrade \
---old-bindir="/usr/lib/postgresql/bin" \
---new-bindir=${PGBINNEW} \
---old-datadir=${PGDATAOLD} \
---new-datadir=${PGDATANEW} \
---jobs="${WORKERS}" \
---old-options='-c config_file=/etc/postgresql/postgresql.conf' \
---new-options="-c data_directory=${PGDATANEW}"
-EOF
-)
-
-systemctl stop postgresql
-su -c "$UPGRADE_COMMAND" -s $SHELL postgres
-
-# copying custom configurations
-mkdir -p $MOUNT_POINT/conf
-cp /etc/postgresql-custom/* $MOUNT_POINT/conf/
-
-cleanup
+initiate_upgrade >> /var/log/pg-upgrade-initiate.log 2>&1
+echo "Upgrade initiate job completed "
