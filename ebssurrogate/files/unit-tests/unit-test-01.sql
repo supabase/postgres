6587eca5d0c1ca2715a6e6ca4923f5de4381d588
Lakshmipathi <lakshmi@supabase.io>
2022-09-21 11:49:34 +0530

    test: Update basic unit test sql (#300)
    Signed-off-by: Lakshmipathi <lakshmi@supabase.io>

--- ebssurrogate/files/unit-tests/unit-test-01.sql
+++ ebssurrogate/files/unit-tests/unit-test-01.sql
@@ -1,5 +1,5 @@
 BEGIN;
-SELECT plan( 22 );
+SELECT plan( 7 );
 
 -- Check installed extensions
 SELECT extensions_are(
@@ -7,10 +7,6 @@ SELECT extensions_are(
     'plpgsql', 
     'pg_stat_statements', 
     'pgsodium',
-    'uuid-ossp',
-    'pgcrypto',
-    'pgjwt',
-    'pg_graphql',
     'pgtap' ]
 );
 
@@ -21,27 +17,6 @@ SELECT has_schema('information_schema');
 SELECT has_schema('pgsodium');
 SELECT has_schema('pgsodium_masks');
 SELECT has_schema('public');
-SELECT has_schema('extensions');
-SELECT has_schema('storage');
-SELECT has_schema('auth');
-SELECT has_schema('realtime');
-SELECT has_schema('graphql_public');
-SELECT has_schema('graphql');
-
--- Check roles
-SELECT has_role('anon');
-SELECT has_role('dashboard_user');
-SELECT has_role('service_role');
-SELECT has_role('supabase_admin');
-SELECT has_role('supabase_auth_admin');
-SELECT has_role('supabase_storage_admin');
-
--- Check auth schema relations
-SELECT fk_ok('auth','identities','user_id','auth','users','id');
-SELECT fk_ok('auth','sessions','user_id','auth','users','id');
-
--- Check storage schema relations
-SELECT fk_ok('storage','objects','bucket_id','storage','buckets','id');
 
 SELECT * from finish();
 ROLLBACK;
