b928a54f4bd9cab9db6b6a0903eadb23fca5cd23
Paul Cioanca <paul.cioanca@supabase.io>
2022-10-02 23:26:13 +0300

    chore: updated readonly mode management script

--- ansible/files/admin_api_scripts/set_readonly_mode.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-
-MODE=$1
-psql -h localhost -U supabase_admin -d postgres -c "ALTER SYSTEM SET default_transaction_read_only to ${MODE};"
-psql -h localhost -U supabase_admin -d postgres -c "SELECT pg_reload_conf();"
