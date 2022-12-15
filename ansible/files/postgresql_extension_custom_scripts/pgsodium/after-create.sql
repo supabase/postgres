ba8ec722ea7596d4b2f9a005485c50f461eab88f
Bobbie Soedirgo <bobbie@soedirgo.dev>
2022-12-15 16:49:19 +0800

    fix: grant pgsodium functions to service_role
    Using supautils's extension custom scripts. The script will be run on
    `create extension pgsodium`.

---
+++ ansible/files/postgresql_extension_custom_scripts/pgsodium/after-create.sql
@@ -0,0 +1,4 @@
+grant execute on function pgsodium.crypto_aead_det_decrypt(bytea, bytea, uuid, bytea) to service_role;
+grant execute on function pgsodium.crypto_aead_det_encrypt(bytea, bytea, uuid, bytea) to service_role;
+grant execute on function pgsodium.crypto_aead_det_keygen to service_role;
+grant execute on function pgsodium.crypto_aead_det_noncegen to service_role;
