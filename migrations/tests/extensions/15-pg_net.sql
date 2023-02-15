c7355585bc8ee50e1f868dbecccc00fffa4d6940
Qiao Han <qiao@supabase.io>
2023-02-15 15:49:41 +0800

    chore: add tests for creating extensions

---
+++ migrations/tests/extensions/15-pg_net.sql
@@ -0,0 +1,1 @@
+create extension if not exists pg_net with schema "extensions";
---
+++ migrations/tests/extensions/18-pgsodium.sql
@@ -0,0 +1,2 @@
+create schema if not exists "pgsodium";
+create extension if not exists pgsodium with schema "pgsodium";
