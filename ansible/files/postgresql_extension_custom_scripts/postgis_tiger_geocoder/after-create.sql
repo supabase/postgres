16598d78bcb784f55fb7a918b159ae2a418d9400
Qiao Han <qiao@supabase.io>
2023-02-24 22:25:28 +0800

    chore: add grants for tiger_data

--- ansible/files/postgresql_extension_custom_scripts/postgis_tiger_geocoder/after-create.sql
+++ ansible/files/postgresql_extension_custom_scripts/postgis_tiger_geocoder/after-create.sql
@@ -1,9 +1,9 @@
-grant usage on schema tiger to postgres with grant option;
+grant usage on schema tiger, tiger_data to postgres with grant option;
 
-grant all on all tables in schema tiger to postgres with grant option;
-grant all on all routines in schema tiger to postgres with grant option;
-grant all on all sequences in schema tiger to postgres with grant option;
+grant all on all tables in schema tiger, tiger_data to postgres with grant option;
+grant all on all routines in schema tiger, tiger_data to postgres with grant option;
+grant all on all sequences in schema tiger, tiger_data to postgres with grant option;
 
-alter default privileges in schema tiger grant all on tables to postgres with grant option;
-alter default privileges in schema tiger grant all on routines to postgres with grant option;
-alter default privileges in schema tiger grant all on sequences to postgres with grant option;
+alter default privileges in schema tiger, tiger_data grant all on tables to postgres with grant option;
+alter default privileges in schema tiger, tiger_data grant all on routines to postgres with grant option;
+alter default privileges in schema tiger, tiger_data grant all on sequences to postgres with grant option;
--- migrations/tests/extensions/01-postgis.sql
+++ migrations/tests/extensions/01-postgis.sql
@@ -7,20 +7,20 @@ BEGIN;
 create extension if not exists address_standardizer with schema extensions;
 create extension if not exists postgis_tiger_geocoder cascade;
 -- \ir ansible/files/postgresql_extension_custom_scripts/postgis_tiger_geocoder/after-create.sql
-grant usage on schema tiger to postgres with grant option;
-grant all privileges on all tables in schema tiger to postgres with grant option;
-grant all privileges on all routines in schema tiger to postgres with grant option;
-grant all privileges on all sequences in schema tiger to postgres with grant option;
-alter default privileges in schema tiger grant all on tables to postgres with grant option;
-alter default privileges in schema tiger grant all on routines to postgres with grant option;
-alter default privileges in schema tiger grant all on sequences to postgres with grant option;
+grant usage on schema tiger, tiger_data to postgres with grant option;
+grant all privileges on all tables in schema tiger, tiger_data to postgres with grant option;
+grant all privileges on all routines in schema tiger, tiger_data to postgres with grant option;
+grant all privileges on all sequences in schema tiger, tiger_data to postgres with grant option;
+alter default privileges in schema tiger, tiger_data grant all on tables to postgres with grant option;
+alter default privileges in schema tiger, tiger_data grant all on routines to postgres with grant option;
+alter default privileges in schema tiger, tiger_data grant all on sequences to postgres with grant option;
 -- postgres role should have access
 set local role postgres;
 select tiger.pprint_addy(tiger.pagc_normalize_address('710 E Ben White Blvd, Austin, TX 78704'));
 -- other roles can be granted access
-grant usage on schema tiger to authenticated;
-grant select on all tables in schema tiger to authenticated;
-grant execute on all routines in schema tiger to authenticated;
+grant usage on schema tiger, tiger_data to authenticated;
+grant select on all tables in schema tiger, tiger_data to authenticated;
+grant execute on all routines in schema tiger, tiger_data to authenticated;
 -- authenticated role should have access now
 set local role authenticated;
 select tiger.pprint_addy(tiger.pagc_normalize_address('710 E Ben White Blvd, Austin, TX 78704'));
