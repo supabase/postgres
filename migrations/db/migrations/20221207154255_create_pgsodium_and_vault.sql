ce56df54752860b333841b3744b1925c271494a3
dragarcia <Angelico.delosreyes@gmail.com>
2023-04-04 23:47:00 +0800

    chore: enable Vault (#598)
    * chore: re-enable Vault
    
    * chore: bump version
    
    * chore: version as rc
    
    * fix: formatting
    
    * chore: build test image from branch
    
    * chore: trigger build
    
    * chore: remove branch
    
    * chore: bump version

--- migrations/db/migrations/20221207154255_create_pgsodium_and_vault.sql
+++ migrations/db/migrations/20221207154255_create_pgsodium_and_vault.sql
@@ -10,6 +10,6 @@ grant execute on function pgsodium.crypto_aead_det_decrypt(bytea, bytea, uuid, b
 grant execute on function pgsodium.crypto_aead_det_encrypt(bytea, bytea, uuid, bytea) to service_role;
 grant execute on function pgsodium.crypto_aead_det_keygen to service_role;
 
--- create extension if not exists supabase_vault;
+create extension if not exists supabase_vault;
 
 -- migrate:down
