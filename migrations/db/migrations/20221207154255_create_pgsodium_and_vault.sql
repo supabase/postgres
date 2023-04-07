13eb41d1a5a25a1cdd5a956f98b6829ded4427ca
dragarcia <Angelico.delosreyes@gmail.com>
2023-04-07 11:19:33 +0800

    chore: enable Vault with safety measures (#611)
    * chore: re-enable Vault
    
    * chore: bump version
    
    * chore: version as rc
    
    * fix: formatting
    
    * chore: build test image from branch
    
    * chore: trigger build
    
    * chore: remove branch
    
    * chore: bump version
    
    * chore: add safeguards when enabling Vaault
    
    * chore: revert changes
    
    * chore: safeguard both pgsodium and vault
    
    * chore: bump version

--- migrations/db/migrations/20221207154255_create_pgsodium_and_vault.sql
+++ migrations/db/migrations/20221207154255_create_pgsodium_and_vault.sql
@@ -1,15 +1,39 @@
 -- migrate:up
 
-create extension if not exists pgsodium;
+DO $$
+DECLARE
+  pgsodium_exists boolean;
+  vault_exists boolean;
+BEGIN
+  pgsodium_exists = (
+    select count(*) = 1 
+    from pg_available_extensions 
+    where name = 'pgsodium'
+  );
+  
+  vault_exists = (
+      select count(*) = 1 
+      from pg_available_extensions 
+      where name = 'supabase_vault'
+  );
 
-grant pgsodium_keyiduser to postgres with admin option;
-grant pgsodium_keyholder to postgres with admin option;
-grant pgsodium_keymaker  to postgres with admin option;
+  IF pgsodium_exists 
+  THEN
+    create extension if not exists pgsodium;
 
-grant execute on function pgsodium.crypto_aead_det_decrypt(bytea, bytea, uuid, bytea) to service_role;
-grant execute on function pgsodium.crypto_aead_det_encrypt(bytea, bytea, uuid, bytea) to service_role;
-grant execute on function pgsodium.crypto_aead_det_keygen to service_role;
+    grant pgsodium_keyiduser to postgres with admin option;
+    grant pgsodium_keyholder to postgres with admin option;
+    grant pgsodium_keymaker  to postgres with admin option;
 
--- create extension if not exists supabase_vault;
+    grant execute on function pgsodium.crypto_aead_det_decrypt(bytea, bytea, uuid, bytea) to service_role;
+    grant execute on function pgsodium.crypto_aead_det_encrypt(bytea, bytea, uuid, bytea) to service_role;
+    grant execute on function pgsodium.crypto_aead_det_keygen to service_role;
+
+    IF vault_exists
+    THEN
+      create extension if not exists supabase_vault;
+    END IF;
+  END IF;
+END $$;
 
 -- migrate:down
