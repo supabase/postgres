a892b059d1177c553edcec31906c6952eef32f58
Div Arora <root@darora.com>
2023-07-21 11:19:46 +0800

    fix: ensure that the test slot gets dropped
    Based on a quick test, a rollback of the txn does not remove the
    logical replication slot that gets created.

--- migrations/tests/extensions/11-wal2json.sql
+++ migrations/tests/extensions/11-wal2json.sql
@@ -1,4 +1,5 @@
 BEGIN;
 select pg_drop_replication_slot(slot_name) from pg_replication_slots where slot_name = 'test_slot';
 select * from pg_create_logical_replication_slot('test_slot', 'wal2json');
+select pg_drop_replication_slot(slot_name) from pg_replication_slots where slot_name = 'test_slot';
 ROLLBACK;
