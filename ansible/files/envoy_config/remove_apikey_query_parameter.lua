245f5f256830c6b544b9409a608c0eb5caa4cb88
Beng Eu <beng@bengeu.com>
2023-12-05 13:08:56 +0800

    docs: add comment explaining why `apikey` query parameter needs to be removed

--- ansible/files/envoy_config/remove_apikey_query_parameter.lua
+++ ansible/files/envoy_config/remove_apikey_query_parameter.lua
@@ -1,5 +1,8 @@
 function envoy_on_request(request_handle)
-    local path = request_handle:headers():get(":path")
+  local path = request_handle:headers():get(":path")
 
-    request_handle:headers():replace(":path", path:gsub("([&?])apikey=[^&]+&?", "%1"):gsub("&$", ""))
+  -- Remove `apikey` query parameter since PostgREST treats query parameters as conditions.
+  request_handle
+    :headers()
+    :replace(":path", path:gsub("([&?])apikey=[^&]+&?", "%1"):gsub("&$", ""))
 end
