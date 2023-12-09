a18fa2dc807a2e70345680761472fe52aef29706
Han Qiao <qiao@supabase.io>
2023-12-09 10:05:33 +0800

    fix: rebuild orioledb base image (#836)
    * fix: rebuild orioledb base image
    
    * chore: bump orioledb release version
    
    * feat: support disabling s3 backend

--- docker/orioledb/entrypoint.sh
+++ docker/orioledb/entrypoint.sh
@@ -3,19 +3,13 @@ set -eou pipefail
 
 PG_CONF=/etc/postgresql/postgresql.conf
 
-function configure_orioledb {
-  echo "Configuring OrioleDB..."
-
-  sed -i \
-    -e "s|#max_worker_processes = .*|max_worker_processes = 50 # should fit orioledb.s3_num_workers as long as other workers|" \
-    -e "s|#log_min_messages = .*|log_min_messages = debug1 # will log all S3 requests|" \
-    -e "s|#archive_mode = off\(.*\)|archive_mode = on\1|" \
-    "$PG_CONF"
+if [ "${S3_ENABLED:-}" == "true" ]; then
+  echo "Enabling OrioleDB S3 Backend..."
 
   echo "
+archive_mode = on
 archive_library = 'orioledb'
-orioledb.main_buffers = 1GB
-orioledb.undo_buffers = 256MB
+max_worker_processes = 50 # should fit orioledb.s3_num_workers as long as other workers
 orioledb.s3_num_workers = 20 # should be enough for comfortable work
 orioledb.s3_mode = true
 orioledb.s3_host = '$S3_HOST' # replace with your bucket URL, accelerated buckets are recommended
@@ -23,10 +17,20 @@ orioledb.s3_region = '$S3_REGION' # replace with your S3 region
 orioledb.s3_accesskey = '$S3_ACCESS_KEY' # replace with your S3 key
 orioledb.s3_secretkey = '$S3_SECRET_KEY' # replace with your S3 secret key
 " >> "$PG_CONF"
-}
+else
+  echo "Disabling OrioleDB S3 Backend..."
 
-if ! grep -q orioledb "$PG_CONF"; then
-  configure_orioledb
+  sed -i \
+    -e "/^archive_mode = on/d" \
+    -e "/^archive_library = 'orioledb'/d" \
+    -e "/^max_worker_processes = 50/d" \
+    -e "/^orioledb.s3_num_workers = /d" \
+    -e "/^orioledb.s3_mode = /d" \
+    -e "/^orioledb.s3_host = /d" \
+    -e "/^orioledb.s3_region = /d" \
+    -e "/^orioledb.s3_accesskey = /d" \
+    -e "/^orioledb.s3_secretkey = /d" \
+    "$PG_CONF"
 fi
 
-docker-entrypoint.sh "$@"
+orioledb-entrypoint.sh "$@"
