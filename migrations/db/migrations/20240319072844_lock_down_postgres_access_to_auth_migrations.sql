e01b59937731e546288d56707c0428f30b3e5caf
Qiao Han <qiao@supabase.io>
2024-03-20 12:51:44 +0700

    chore: update migration tests

--- migrations/db/migrations/20240319072844_lock_down_postgres_access_to_auth_migrations.sql
+++ migrations/db/migrations/20240319072844_lock_down_postgres_access_to_auth_migrations.sql
@@ -3,8 +3,4 @@ revoke supabase_auth_admin from postgres;
 revoke all on table auth.schema_migrations from postgres;
 grant select on table auth.schema_migrations to postgres;
 
-revoke supabase_storage_admin from postgres;
-revoke all on table storage.migrations from postgres;
-grant select on table storage.migrations to postgres;
-
 -- migrate:down
