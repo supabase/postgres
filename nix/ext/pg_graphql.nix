1d6b93260e2ebb744a149ce6bcd94c94029ba159
samrose <samuel.rose@gmail.com>
2024-04-09 20:08:32 -0400

    fix: use pgrx 0.11.3 on pg_graphql

--- nix/ext/pg_graphql.nix
+++ nix/ext/pg_graphql.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, postgresql, buildPgrxExtension_0_11_3 }:
+{ lib, stdenv, fetchFromGitHub, postgresql, buildPgrxExtension_0_11_3, cargo }:
 
 buildPgrxExtension_0_11_3 rec {
   pname = "pg_graphql";
@@ -9,10 +9,12 @@ buildPgrxExtension_0_11_3 rec {
     owner = "samrose";
     repo = pname;
     rev = "sam/update-pgrx";
-    hash = "sha256-1AiD2iSFmZwC+Zy0x+MABseWCxXRtRY74Dj0oBKet+o=";
+    hash = "sha256-8HM4g7ylBVlPzjr8bBfEM1smK2xRGD040EdfLFhlLRo=";
   };
 
-  cargoHash = "sha256-1Ow4SafWD8z2Oj8KPVv7GQEOQ1TCShhcG7XmQiM9W68=";
+  nativeBuildInputs = [ cargo ];
+  CARGO="${cargo}/bin/cargo";
+  cargoHash = "sha256-E5dMTfFrjZ5ghpr7KEzdBuDvnF+mVjmwMT3CcHQylN4=";
 
   # FIXME (aseipp): disable the tests since they try to install .control
   # files into the wrong spot, aside from that the one main test seems
