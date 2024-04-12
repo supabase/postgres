9780dc39ae9229e4d6b5f3d469631298b348d261
samrose <samuel.rose@gmail.com>
2024-04-12 17:51:04 -0400

    feat: inject aws into container, and signing key for binary cache upload cache if build succeeds, on every run

--- docker/nix/build.sh
+++ docker/nix/build.sh
@@ -1,10 +1,10 @@
 #!/bin/env bash
 set -eou pipefail
-echo "$REGISTRY"
+
 nix --version
 cd /workspace
 nix build .#psql_15/bin -o psql_15
 nix build .#psql_15/docker #just to test the build
-
+nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key psql_15
 
 #a future step nix run .#psql_15/docker.copyToRegistry 
