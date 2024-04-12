5dc91659a5413796742d59f32835ce44722e5520
samrose <samuel.rose@gmail.com>
2024-04-12 19:08:19 -0400

    fix: copy to binary cache

--- docker/nix/build.sh
+++ docker/nix/build.sh
@@ -4,7 +4,7 @@ set -eou pipefail
 nix --version
 cd /workspace
 nix build .#psql_15/bin -o psql_15
-nix build .#psql_15/docker #just to test the build
-nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key psql_15
+nix build .#psql_15/docker
+nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_15
 
 #a future step nix run .#psql_15/docker.copyToRegistry 
