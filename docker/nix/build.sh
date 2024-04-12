eac46c92bcbf08a90d0ef45a0e7c0218f55ab4a3
samrose <samuel.rose@gmail.com>
2024-04-12 14:26:09 -0400

    chore: deprecate push to registry for now, after confirming it works

--- docker/nix/build.sh
+++ docker/nix/build.sh
@@ -5,4 +5,6 @@ nix --version
 cd /workspace
 nix build .#psql_15/bin -o psql_15
 nix build .#psql_15/docker #just to test the build
-nix run .#psql_15/docker.copyToRegistry 
+
+
+#a future step nix run .#psql_15/docker.copyToRegistry 
