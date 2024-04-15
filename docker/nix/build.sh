363dedc0f1327e1f246b80509f176a1d2cfd2e18
samrose <samuel.rose@gmail.com>
2024-04-14 20:55:17 -0400

    feat: include test (nix flake check -L)

--- docker/nix/build.sh
+++ docker/nix/build.sh
@@ -5,6 +5,7 @@ nix --version
 cd /workspace
 nix build .#psql_15/bin -o psql_15
 nix build .#psql_15/docker
+nix flake check -L --all-systems
 nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_15
 
 #a future step nix run .#psql_15/docker.copyToRegistry 
