a33357dffdd730f5a5fc81324c8b228a29c49a48
Tom Ashley <tom.ashley@gmail.com>
2024-05-20 15:24:53 +0100

    fix: add route checking to network fix script

--- ansible/files/systemd-networkd/systemd-networkd-check-and-fix.sh
+++ ansible/files/systemd-networkd/systemd-networkd-check-and-fix.sh
@@ -6,6 +6,15 @@ journalctl --no-pager --unit systemd-networkd --since "1 minutes ago" --grep "Co
 NDISC_ERROR=$?
 
 if systemctl is-active --quiet systemd-networkd.service && [ "${NDISC_ERROR}" == 0 ]; then
-  echo "$(date) systemd-network running but NDisc routes are broken. Restarting systemd.networkd.system"
+  echo "$(date) systemd-network running but NDisc routes are broken. Restarting systemd.networkd.service"
+  /usr/bin/systemctl restart systemd-networkd.service
+  exit  # no need to check further
+fi
+
+# check for routes
+ROUTES=$(ip -6 route list)
+
+if ! echo "${ROUTES}" | grep default >/dev/null || ! echo "${ROUTES}" | grep "::1 dev lo">/dev/null; then
+  echo "IPv6 routing table messed up. Restarting systemd.networkd.service"
   /usr/bin/systemctl restart systemd-networkd.service
 fi
