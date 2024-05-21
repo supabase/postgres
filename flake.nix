429d89a8bba2f26f073655a867c6ae0240750d92
samrose <samuel.rose@gmail.com>
2024-05-21 10:58:30 -0400

    Sam/pljava nix (#976)
    * feat: package pljava
    
    * feat: pljava psql nix package
    
    * feat: package and smoke test pljava extension into the bundle
    run on openjdk11
    
    * chore: cleanup nix expression

--- flake.nix
+++ flake.nix
@@ -128,6 +128,7 @@
           ./nix/ext/wrappers/default.nix
           ./nix/ext/supautils.nix
           ./nix/ext/plv8.nix
+          ./nix/ext/pljava.nix
         ];
 
         #Where we import and build the orioledb extension, we add on our custom extensions
@@ -412,7 +413,8 @@
                 --subst-var-by 'PGSQL_SUPERUSER' '${pgsqlSuperuser}' \
                 --subst-var-by 'PSQL15_BINDIR' '${basePackages.psql_15.bin}' \
                 --subst-var-by 'PSQL_CONF_FILE' '${configFile}' \
-                --subst-var-by 'PGSODIUM_GETKEY' '${getkeyScript}'
+                --subst-var-by 'PGSODIUM_GETKEY' '${getkeyScript}' \
+                --subst-var-by 'LIBJVM_LOCATION' '${pkgs.openjdk11}/lib/openjdk/lib/server/libjvm.so'
 
               chmod +x $out/bin/start-postgres-server
             '';
@@ -475,13 +477,16 @@
           pkgs.runCommand "postgres-${pgpkg.version}-check-harness"
             {
               nativeBuildInputs = with pkgs; [ coreutils bash pgpkg pg_prove procps ];
+              propagatedBuildInputs = with pkgs; [ openjdk11 ];
             } ''
             export PGDATA=/tmp/pgdata
             mkdir -p $PGDATA
             initdb --locale=C
 
             substitute ${./nix/tests/postgresql.conf.in} $PGDATA/postgresql.conf \
-              --subst-var-by PGSODIUM_GETKEY_SCRIPT "${./nix/tests/util/pgsodium_getkey.sh}"
+              --subst-var-by PGSODIUM_GETKEY_SCRIPT "${./nix/tests/util/pgsodium_getkey.sh}" \
+              --subst-var-by PLJAVA_LIBJVM_LOCATION "${pkgs.openjdk11}/lib/openjdk/lib/server/libjvm.so"
+
 
             postgres -k /tmp >logfile 2>&1 &
             sleep 2
