291755db44d8e4dce2df0e8fa60bc2f4baab13ca
Bobbie Soedirgo <bobbie@soedirgo.dev>
2024-06-26 13:16:46 +0800

    feat: grant predefined roles to postgres
    Decouples some changes from
    https://github.com/supabase/postgres/pull/994/files
    
    `postgres` can already grant these roles to itself; granting these by
    default removes one step if users need to read Storage migrations, kill
    running queries from non-superusers, etc.

--- migrations/tests/database/privs.sql
+++ migrations/tests/database/privs.sql
@@ -1,4 +1,3 @@
-
 SELECT database_privs_are(
     'postgres', 'postgres', ARRAY['CONNECT', 'TEMPORARY', 'CREATE']
 );
@@ -28,3 +27,7 @@ SELECT schema_privs_are('extensions', 'postgres', array['CREATE', 'USAGE']);
 SELECT schema_privs_are('extensions', 'anon', array['USAGE']);
 SELECT schema_privs_are('extensions', 'authenticated', array['USAGE']);
 SELECT schema_privs_are('extensions', 'service_role', array['USAGE']);
+
+-- Role memberships
+SELECT is_member_of('pg_read_all_data', 'postgres');
+SELECT is_member_of('pg_signal_backend', 'postgres');
