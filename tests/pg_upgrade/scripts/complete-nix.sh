f7c1454b47bcd45234b192b65667184292300834
Sam Rose <samuel@supabase.io>
2024-07-16 20:13:58 +0000

    test: mod complete script to testing conditions

--- tests/pg_upgrade/scripts/complete-nix.sh
+++ tests/pg_upgrade/scripts/complete-nix.sh
@@ -71,49 +71,6 @@ EOF
         run_sql -c "$RECREATE_PG_CRON_QUERY"
     fi
 }
-
-function complete_pg_upgrade {
-    if [ -f /tmp/pg-upgrade-status ]; then
-        echo "Upgrade job already started. Bailing."
-        exit 0
-    fi
-
-    echo "running" > /tmp/pg-upgrade-status
-
-    echo "1. Mounting data disk"
-    retry 8 mount -a -v
-
-    # copying custom configurations
-    echo "2. Copying custom configurations"
-    retry 3 copy_configs
-
-    echo "3. Starting postgresql"
-    retry 3 service postgresql start
-
-    echo "4. Running generated SQL files"
-    retry 3 run_generated_sql
-
-    echo "4.1. Applying patches"
-    execute_patches || true
-
-    run_sql -c "ALTER USER postgres WITH NOSUPERUSER;"
-
-    echo "4.2. Applying authentication scheme updates"
-    retry 3 apply_auth_scheme_updates
-
-    sleep 5
-
-    echo "5. Restarting postgresql"
-    retry 3 service postgresql restart
-
-    echo "5.1. Restarting gotrue and postgrest"
-    retry 3 service gotrue restart
-    retry 3 service postgrest restart
-
-    echo "6. Starting vacuum analyze"
-    retry 3 start_vacuum_analyze
-}
-
 function copy_configs {
     cp -R /data/conf/* /etc/postgresql-custom/
     chown -R postgres:postgres /var/lib/postgresql/data
@@ -147,7 +104,52 @@ function start_vacuum_analyze {
     echo "Upgrade job completed"
 }
 
+
+function complete_pg_upgrade {
+    # if [ -f /tmp/pg-upgrade-status ]; then
+    #     echo "Upgrade job already started. Bailing."
+    #     exit 0
+    # fi
+
+    echo "running" > /tmp/pg-upgrade-status
+
+    echo "1. Mounting data disk"
+    retry 8 mount -a -v
+
+    # copying custom configurations
+    # already done
+    # echo "2. Copying custom configurations"
+    # retry 3 copy_configs
+
+    # echo "3. Starting postgresql"
+    # retry 3 service postgresql start
+
+    echo "4. Running generated SQL files"
+    retry 3 run_generated_sql
+
+    echo "4.1. Applying patches"
+    execute_patches || true
+
+    run_sql -c "ALTER USER postgres WITH NOSUPERUSER;"
+
+    echo "4.2. Applying authentication scheme updates"
+    retry 3 apply_auth_scheme_updates
+
+    sleep 5
+
+    # echo "5. Restarting postgresql"
+    # retry 3 service postgresql restart
+
+    # echo "5.1. Restarting gotrue and postgrest"
+    # retry 3 service gotrue restart
+    # retry 3 service postgrest restart
+
+    echo "6. Starting vacuum analyze"
+    retry 3 start_vacuum_analyze
+}
+
+
 trap cleanup ERR
 
 
-complete_pg_upgrade >> $LOG_FILE 2>&1 &
+complete_pg_upgrade
