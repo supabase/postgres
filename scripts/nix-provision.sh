7eabe03aa5bb5440bd70b56b99fe540a46bf727c
samrose <samuel.rose@gmail.com>
2024-07-18 11:34:44 -0400

    Sam/timescale and wrappers (#1052)
    * fix: was using the wrong sha256 hash for version
    
    * chore: updating wrappers version
    
    * itests: make sure we run the current commit on psql bundle test
    
    ---------
    
    Co-authored-by: Sam Rose <samuel@supabase.io>

--- scripts/nix-provision.sh
+++ scripts/nix-provision.sh
@@ -25,15 +25,17 @@ function install_nix() {
 
 
 function execute_stage2_playbook {
-
-sudo tee /etc/ansible/ansible.cfg <<EOF
+    sudo tee /etc/ansible/ansible.cfg <<EOF
 [defaults]
 callbacks_enabled = timer, profile_tasks, profile_roles
 EOF
-	sed -i 's/- hosts: all/- hosts: localhost/' /tmp/ansible-playbook/ansible/playbook.yml
-	# Run Ansible playbook
-	export ANSIBLE_LOG_PATH=/tmp/ansible.log && export ANSIBLE_REMOTE_TEMP=/tmp
-	ansible-playbook /tmp/ansible-playbook/ansible/playbook.yml --extra-vars '{"nixpkg_mode": false, "stage2_nix": true, "debpkg_mode": false}' $ARGS
+    sed -i 's/- hosts: all/- hosts: localhost/' /tmp/ansible-playbook/ansible/playbook.yml
+    # Run Ansible playbook
+    export ANSIBLE_LOG_PATH=/tmp/ansible.log && export ANSIBLE_REMOTE_TEMP=/tmp
+    ansible-playbook /tmp/ansible-playbook/ansible/playbook.yml \
+        --extra-vars '{"nixpkg_mode": false, "stage2_nix": true, "debpkg_mode": false}' \
+        --extra-vars "git_commit_sha=${GIT_SHA}" \
+        $ARGS
 }
 
 
