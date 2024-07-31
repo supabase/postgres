c0eb58264e05ae3d759ac8ab16e34fd9cdad45ee
Sam Rose <samuel@supabase.io>
2024-07-31 09:36:52 -0400

    feat: allow postgres to run on darwin

--- flake.nix
+++ flake.nix
@@ -15,6 +15,7 @@
       ourSystems = with flake-utils.lib; [
         system.x86_64-linux
         system.aarch64-linux
+        system.aarch64-darwin
       ];
     in
     flake-utils.lib.eachSystem ourSystems (system:
@@ -130,7 +131,7 @@
           ./nix/ext/pg_tle.nix
           ./nix/ext/wrappers/default.nix
           ./nix/ext/supautils.nix
-          ./nix/ext/plv8.nix
+          #./nix/ext/plv8.nix
         ];
 
         #Where we import and build the orioledb extension, we add on our custom extensions
