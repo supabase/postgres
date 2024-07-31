97552e887967b8088e4b532a966aa41ed0c312fd
Sam Rose <samuel@supabase.io>
2024-07-31 11:43:16 -0400

    chore: no need to build docker here

--- docker/nix/build_nix.sh
+++ docker/nix/build_nix.sh
@@ -4,6 +4,5 @@ set -eou pipefail
 nix --version
 cd /workspace
 nix build .#psql_15/bin -o psql_15
-nix build .#psql_15/docker
 nix flake check -L 
 nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_15
