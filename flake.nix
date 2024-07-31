78404b4f687760f59fc7050e1bf34293f54fb5a9
Dominic Mills <dominic.millz27@gmail.com>
2024-07-31 10:35:33 -0500

    Added the Garnix cache to the flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   description = "Prototype tooling for deploying PostgreSQL";
 
+  nixConfig = {
+    extra-substituters = ["https://cache.garnix.io"];
+    extra-trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
