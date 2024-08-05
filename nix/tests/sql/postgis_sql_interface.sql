75ffaabeb32219d4e8abeb23b7d4f642fe4bf51f
Oliver Rice <github@oliverrice.com>
2024-08-05 16:30:47 -0500

    whitesapce cleanup

--- nix/tests/sql/postgis_sql_interface.sql
+++ nix/tests/sql/postgis_sql_interface.sql
@@ -5,12 +5,12 @@ select
     pg_catalog.pg_get_function_result(p.oid) as return_type
 from
     pg_catalog.pg_proc p
-	join pg_catalog.pg_namespace n
-		on n.oid = p.pronamespace
-	join pg_catalog.pg_depend d
-		on d.objid = p.oid
-	join pg_catalog.pg_extension e
-		on e.oid = d.refobjid
+    join pg_catalog.pg_namespace n
+        on n.oid = p.pronamespace
+    join pg_catalog.pg_depend d
+        on d.objid = p.oid
+    join pg_catalog.pg_extension e
+        on e.oid = d.refobjid
 where
     e.extname = 'postgis' -- replace with your extension name
     and n.nspname = 'public'
