786534a19dc1bf2347468f500d906e74d83d1053
samrose <samuel.rose@gmail.com>
2024-08-12 14:32:55 -0400

    fix: we need to produce a series of migration files on pkg build although they are all a copy of the latest

--- nix/ext/wrappers/default.nix
+++ nix/ext/wrappers/default.nix
@@ -7,43 +7,41 @@
 , buildPgrxExtension_0_11_3
 , cargo
 , darwin
+, jq
 }:
 
+let
+  gitTags = builtins.fromJSON (builtins.readFile (builtins.fetchurl {
+    url = "https://api.github.com/repos/supabase/wrappers/tags";
+    sha256 = "0am40yspir70wp8pik1c7qmfvbby3nyxza115pi9klp6fyv2s93j"; # Replace with actual hash
+  }));
+in
 buildPgrxExtension_0_11_3 rec {
   pname = "supabase-wrappers";
   version = "0.4.1";
   inherit postgresql;
-
   src = fetchFromGitHub {
     owner = "supabase";
     repo = "wrappers";
     rev = "v${version}";
     hash = "sha256-AU9Y43qEMcIBVBThu+Aor1HCtfFIg+CdkzK9IxVdkzM=";
   };
-
-  nativeBuildInputs = [ pkg-config cargo ];
-
-  buildInputs = [ openssl ] ++ lib.optionals (stdenv.isDarwin)  [ 
+  nativeBuildInputs = [ pkg-config cargo jq ];
+  buildInputs = [ openssl ] ++ lib.optionals (stdenv.isDarwin) [ 
     darwin.apple_sdk.frameworks.CoreFoundation 
     darwin.apple_sdk.frameworks.Security 
     darwin.apple_sdk.frameworks.SystemConfiguration 
   ];
-
-  # Needed to get openssl-sys to use pkg-config.
   OPENSSL_NO_VENDOR = 1;
   
   CARGO="${cargo}/bin/cargo";
-
   cargoLock = {
-    #TODO when we move to newer versions this lockfile will need to be sourced
-    # from ${src}/Cargo.lock
     lockFile = "${src}/Cargo.lock";
     outputHashes = {
       "clickhouse-rs-1.0.0-alpha.1" = "sha256-0zmoUo/GLyCKDLkpBsnLAyGs1xz6cubJhn+eVqMEMaw=";
     };
   };
   postPatch = "cp ${cargoLock.lockFile} Cargo.lock";
-
   buildAndTestSubdir = "wrappers";
   buildFeatures = [
     "helloworld_fdw"
@@ -60,11 +58,32 @@ buildPgrxExtension_0_11_3 rec {
     "cognito_fdw"
     "wasm_fdw"
   ];
-
-  # FIXME (aseipp): disable the tests since they try to install .control
-  # files into the wrong spot, aside from that the one main test seems
-  # to work, though
   doCheck = false;
+
+  preBuild = ''
+    echo "Processing git tags..."
+    echo '${builtins.toJSON gitTags}' | ${jq}/bin/jq -r '.[].name' | sort -rV > git_tags.txt
+  '';
+
+  postInstall = ''
+    echo "Creating SQL files for previous versions..."
+    current_version="${version}"
+    sql_file="$out/share/postgresql/extension/wrappers--$current_version.sql"
+    
+    if [ -f "$sql_file" ]; then
+      while read -r tag; do
+        tag_version=$(echo "$tag" | sed 's/^v//')
+        if [ "$(printf '%s\n' "$tag_version" "$current_version" | sort -V | head -n1)" = "$tag_version" ] && [ "$tag_version" != "$current_version" ]; then
+          new_file="$out/share/postgresql/extension/wrappers--$tag_version--$current_version.sql"
+          echo "Creating $new_file"
+          cp "$sql_file" "$new_file"
+        fi
+      done < git_tags.txt
+    else
+      echo "Warning: $sql_file not found"
+    fi
+    rm git_tags.txt
+  '';
 
   meta = with lib; {
     description = "Various Foreign Data Wrappers (FDWs) for PostreSQL";
