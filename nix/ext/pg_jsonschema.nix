3c208ac8438a532c4db626f82a4174c16b924d29
Paul Cioanca <paul.cioanca@supabase.io>
2024-08-21 17:01:08 +0300

    chore: consolidate `release/15.6-lw12` into `develop` (#1142)
    Co-authored-by: Paul Cioanca <paul.cioanca@supabase.io>
    Co-authored-by: Sam Rose <samuel@supabase.io>
    Co-authored-by: Div Arora <root@darora.com>
    Co-authored-by: Div Arora <darora@users.noreply.github.com>
    Co-authored-by: samrose <samuel.rose@gmail.com>

--- nix/ext/pg_jsonschema.nix
+++ nix/ext/pg_jsonschema.nix
@@ -14,7 +14,10 @@ buildPgrxExtension_0_11_3 rec {
 
   nativeBuildInputs = [ cargo ];
   buildInputs = [ postgresql ];
-  
+  # update the following array when the pg_jsonschema version is updated
+  # required to ensure that extensions update scripts from previous versions are generated
+
+  previousVersions = ["0.3.0" "0.2.0" "0.1.4" "0.1.4" "0.1.2" "0.1.1" "0.1.0"];
   CARGO="${cargo}/bin/cargo";
   env = lib.optionalAttrs stdenv.isDarwin {
     POSTGRES_LIB = "${postgresql}/lib";
@@ -25,6 +28,31 @@ buildPgrxExtension_0_11_3 rec {
   # FIXME (aseipp): testsuite tries to write files into /nix/store; we'll have
   # to fix this a bit later.
   doCheck = false;
+
+  preBuild = ''
+    echo "Processing git tags..."
+    echo '${builtins.concatStringsSep "," previousVersions}' | sed 's/,/\n/g' > git_tags.txt
+  '';
+
+  postInstall = ''
+    echo "Creating SQL files for previous versions..."
+    current_version="${version}"
+    sql_file="$out/share/postgresql/extension/pg_jsonschema--$current_version.sql"
+    
+    if [ -f "$sql_file" ]; then
+      while read -r previous_version; do
+        if [ "$(printf '%s\n' "$previous_version" "$current_version" | sort -V | head -n1)" = "$previous_version" ] && [ "$previous_version" != "$current_version" ]; then
+          new_file="$out/share/postgresql/extension/pg_jsonschema--$previous_version--$current_version.sql"
+          echo "Creating $new_file"
+          cp "$sql_file" "$new_file"
+        fi
+      done < git_tags.txt
+    else
+      echo "Warning: $sql_file not found"
+    fi
+    rm git_tags.txt
+  '';
+
 
   meta = with lib; {
     description = "JSON Schema Validation for PostgreSQL";
--- nix/ext/wrappers/default.nix
+++ nix/ext/wrappers/default.nix
@@ -7,43 +7,38 @@
 , buildPgrxExtension_0_11_3
 , cargo
 , darwin
+, jq
 }:
 
 buildPgrxExtension_0_11_3 rec {
   pname = "supabase-wrappers";
   version = "0.4.1";
+  # update the following array when the wrappers version is updated
+  # required to ensure that extensions update scripts from previous versions are generated
+  previousVersions = ["0.4.0" "0.3.1" "0.3.0" "0.2.0" "0.1.19" "0.1.18" "0.1.17" "0.1.16" "0.1.15" "0.1.14" "0.1.12" "0.1.11" "0.1.10" "0.1.9" "0.1.8" "0.1.7" "0.1.6" "0.1.5" "0.1.4" "0.1.1" "0.1.0"];
   inherit postgresql;
-
   src = fetchFromGitHub {
     owner = "supabase";
     repo = "wrappers";
     rev = "v${version}";
     hash = "sha256-AU9Y43qEMcIBVBThu+Aor1HCtfFIg+CdkzK9IxVdkzM=";
   };
-
   nativeBuildInputs = [ pkg-config cargo ];
-
-  buildInputs = [ openssl ] ++ lib.optionals (stdenv.isDarwin)  [ 
+  buildInputs = [ openssl ] ++ lib.optionals (stdenv.isDarwin) [ 
     darwin.apple_sdk.frameworks.CoreFoundation 
     darwin.apple_sdk.frameworks.Security 
     darwin.apple_sdk.frameworks.SystemConfiguration 
   ];
-
-  # Needed to get openssl-sys to use pkg-config.
   OPENSSL_NO_VENDOR = 1;
   
   CARGO="${cargo}/bin/cargo";
-
   cargoLock = {
-    #TODO when we move to newer versions this lockfile will need to be sourced
-    # from ${src}/Cargo.lock
     lockFile = "${src}/Cargo.lock";
     outputHashes = {
       "clickhouse-rs-1.0.0-alpha.1" = "sha256-0zmoUo/GLyCKDLkpBsnLAyGs1xz6cubJhn+eVqMEMaw=";
     };
   };
   postPatch = "cp ${cargoLock.lockFile} Cargo.lock";
-
   buildAndTestSubdir = "wrappers";
   buildFeatures = [
     "helloworld_fdw"
@@ -60,11 +55,31 @@ buildPgrxExtension_0_11_3 rec {
     "cognito_fdw"
     "wasm_fdw"
   ];
-
-  # FIXME (aseipp): disable the tests since they try to install .control
-  # files into the wrong spot, aside from that the one main test seems
-  # to work, though
   doCheck = false;
+
+  preBuild = ''
+    echo "Processing git tags..."
+    echo '${builtins.concatStringsSep "," previousVersions}' | sed 's/,/\n/g' > git_tags.txt
+  '';
+
+  postInstall = ''
+    echo "Creating SQL files for previous versions..."
+    current_version="${version}"
+    sql_file="$out/share/postgresql/extension/wrappers--$current_version.sql"
+    
+    if [ -f "$sql_file" ]; then
+      while read -r previous_version; do
+        if [ "$(printf '%s\n' "$previous_version" "$current_version" | sort -V | head -n1)" = "$previous_version" ] && [ "$previous_version" != "$current_version" ]; then
+          new_file="$out/share/postgresql/extension/wrappers--$previous_version--$current_version.sql"
+          echo "Creating $new_file"
+          cp "$sql_file" "$new_file"
+        fi
+      done < git_tags.txt
+    else
+      echo "Warning: $sql_file not found"
+    fi
+    rm git_tags.txt
+  '';
 
   meta = with lib; {
     description = "Various Foreign Data Wrappers (FDWs) for PostreSQL";
