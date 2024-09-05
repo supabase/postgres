56565f31e9b41379f307e04e1ea2e7a91ff76a00
Beng Eu <beng@bengeu.com>
2024-09-05 14:52:22 +0800

    fix: update Envoy config to have circuit breaker settings, retries, increased timeouts and remove empty key query params

--- ansible/files/envoy_config/remove_apikey_query_parameter.lua
+++
@@ -1,8 +0,0 @@
-function envoy_on_request(request_handle)
-  local path = request_handle:headers():get(":path")
-
-  -- Remove `apikey` query parameter since PostgREST treats query parameters as conditions.
-  request_handle
-    :headers()
-    :replace(":path", path:gsub("([&?])apikey=[^&]+&?", "%1"):gsub("&$", ""))
-end
