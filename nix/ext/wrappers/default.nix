4f2cee435bfe8dcc6d52398594d0a9248c7c8100
Bo Lu <lv.patrick@gmail.com>
2024-09-05 12:41:01 +1000

    revert nix change

--- nix/ext/wrappers/default.nix
+++ nix/ext/wrappers/default.nix
@@ -12,10 +12,10 @@
 
 buildPgrxExtension_0_11_3 rec {
   pname = "supabase-wrappers";
-  version = "0.4.2";
+  version = "0.4.1";
   # update the following array when the wrappers version is updated
   # required to ensure that extensions update scripts from previous versions are generated
-  previousVersions = ["0.4.1" "0.4.0" "0.3.1" "0.3.0" "0.2.0" "0.1.19" "0.1.18" "0.1.17" "0.1.16" "0.1.15" "0.1.14" "0.1.12" "0.1.11" "0.1.10" "0.1.9" "0.1.8" "0.1.7" "0.1.6" "0.1.5" "0.1.4" "0.1.1" "0.1.0"];
+  previousVersions = ["0.4.0" "0.3.1" "0.3.0" "0.2.0" "0.1.19" "0.1.18" "0.1.17" "0.1.16" "0.1.15" "0.1.14" "0.1.12" "0.1.11" "0.1.10" "0.1.9" "0.1.8" "0.1.7" "0.1.6" "0.1.5" "0.1.4" "0.1.1" "0.1.0"];
   inherit postgresql;
   src = fetchFromGitHub {
     owner = "supabase";
