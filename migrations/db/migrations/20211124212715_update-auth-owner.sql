57c9aa9931ed7d5c8e68e4974f569dc87dbe3dc0
Paul Cioanca <paul.cioanca@supabase.io>
2024-09-09 11:22:27 +0300

    chore: catch grant exceptions per-function

--- migrations/db/migrations/20211124212715_update-auth-owner.sql
+++ migrations/db/migrations/20211124212715_update-auth-owner.sql
@@ -4,9 +4,21 @@
 DO $$
 BEGIN
     ALTER FUNCTION auth.uid owner to supabase_auth_admin;
+EXCEPTION WHEN OTHERS THEN
+    RAISE WARNING 'Error encountered when changing owner of auth.uid to supabase_auth_admin';
+END $$;
+
+DO $$
+BEGIN
     ALTER FUNCTION auth.role owner to supabase_auth_admin;
+EXCEPTION WHEN OTHERS THEN
+    RAISE WARNING 'Error encountered when changing owner of auth.role to supabase_auth_admin';
+END $$;
+
+DO $$
+BEGIN
     ALTER FUNCTION auth.email owner to supabase_auth_admin;
 EXCEPTION WHEN OTHERS THEN
-    RAISE WARNING 'Error encountered when changing owner of auth.uid, auth.role or auth.email to supabase_auth_admin';
+    RAISE WARNING 'Error encountered when changing owner of auth.email to supabase_auth_admin';
 END $$;
 -- migrate:down
