0b94f202ec1b4c90b102bd30f9ce4ea77fbcdb79
Paul Cioanca <paul.cioanca@supabase.io>
2024-09-12 17:02:08 +0300

    fix: continue running migrations when auth.uid/auth.role/auth.email are missing in the database (#1187)

--- migrations/db/migrations/20211124212715_update-auth-owner.sql
+++ migrations/db/migrations/20211124212715_update-auth-owner.sql
@@ -1,8 +1,24 @@
 -- migrate:up
 
 -- update owner for auth.uid, auth.role and auth.email functions
-ALTER FUNCTION auth.uid owner to supabase_auth_admin;
-ALTER FUNCTION auth.role owner to supabase_auth_admin;
-ALTER FUNCTION auth.email owner to supabase_auth_admin;
+DO $$
+BEGIN
+    ALTER FUNCTION auth.uid owner to supabase_auth_admin;
+EXCEPTION WHEN OTHERS THEN
+    RAISE WARNING 'Error encountered when changing owner of auth.uid to supabase_auth_admin';
+END $$;
 
+DO $$
+BEGIN
+    ALTER FUNCTION auth.role owner to supabase_auth_admin;
+EXCEPTION WHEN OTHERS THEN
+    RAISE WARNING 'Error encountered when changing owner of auth.role to supabase_auth_admin';
+END $$;
+
+DO $$
+BEGIN
+    ALTER FUNCTION auth.email owner to supabase_auth_admin;
+EXCEPTION WHEN OTHERS THEN
+    RAISE WARNING 'Error encountered when changing owner of auth.email to supabase_auth_admin';
+END $$;
 -- migrate:down
