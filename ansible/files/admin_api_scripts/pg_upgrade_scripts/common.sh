27324c20990e7866958b64492daf2bd0821a3b59
Bobbie Soedirgo <bobbie@soedirgo.dev>
2024-09-12 15:40:31 +0100

    fix: disable client connections during upgrade

--- ansible/files/admin_api_scripts/pg_upgrade_scripts/common.sh
+++ ansible/files/admin_api_scripts/pg_upgrade_scripts/common.sh
@@ -88,6 +88,10 @@ CI_start_postgres() {
 
 swap_postgres_and_supabase_admin() {
     run_sql <<'EOSQL'
+alter database postgres connection limit 0;
+select pg_terminate_backend(pid) from pg_stat_activity where backend_type = 'client backend' and pid != pg_backend_pid();
+EOSQL
+    run_sql <<'EOSQL'
 set statement_timeout = '300s';
 begin;
 create role supabase_tmp superuser;
@@ -508,6 +512,8 @@ begin
   end if;
 end
 $$;
+
+alter database postgres connection limit -1;
 
 set session authorization supabase_admin;
 drop role supabase_tmp;
