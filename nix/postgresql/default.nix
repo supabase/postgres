d8e630887914c9e98d9d693049ac1f25c10b1210
samrose <samuel.rose@gmail.com>
2024-09-13 12:01:25 -0400

    feat: bringing postgres package into our package set (#1195)
    * feat: bringing postgres package into our package set
    
    * chore: on macos we need to set specific ports for the build
    
    ---------
    
    Co-authored-by: Sam Rose <samuel@supabase.io>

---
+++ nix/postgresql/default.nix
@@ -0,0 +1,20 @@
+self:
+let
+  #adapted from the postgresql nixpkgs package
+  versions = {
+    postgresql_15 = ./15.nix;
+  };
+
+  mkAttributes = jitSupport:
+    self.lib.mapAttrs' (version: path:
+      let
+        attrName = if jitSupport then "${version}_jit" else version;
+      in
+      self.lib.nameValuePair attrName (import path {
+        inherit jitSupport self;
+      })
+    ) versions;
+
+in
+# variations without and with JIT
+(mkAttributes false) // (mkAttributes true)
