05ee8f3766ff0e760f07c81be260fb83ca295011
Sam Rose <samuel@supabase.io>
2024-10-07 10:59:59 -0400

    feat: pgrouting 15/16 on all supported platforms

--- nix/ext/pgrouting.nix
+++ nix/ext/pgrouting.nix
@@ -14,10 +14,39 @@ stdenv.mkDerivation rec {
     hash   = "sha256-QC77AnPGpPQGEWi6JtJdiNsB2su5+aV2pKg5ImR2B0k=";
   };
 
+  #disable compile time warnings for incompatible pointer types only on macos and pg16
+  NIX_CFLAGS_COMPILE = lib.optionalString (stdenv.isDarwin && lib.versionAtLeast postgresql.version "16") 
+  "-Wno-error=int-conversion -Wno-error=incompatible-pointer-types";
+
+  cmakeFlags = [
+    "-DPOSTGRESQL_VERSION=${postgresql.version}"
+  ] ++ lib.optionals (stdenv.isDarwin && lib.versionAtLeast postgresql.version "16")  [
+    "-DCMAKE_MACOSX_RPATH=ON"
+    "-DCMAKE_SHARED_MODULE_SUFFIX=.dylib"
+    "-DCMAKE_SHARED_LIBRARY_SUFFIX=.dylib"
+  ];
+
+  preConfigure = lib.optionalString (stdenv.isDarwin && lib.versionAtLeast postgresql.version "16") ''
+    export DLSUFFIX=.dylib
+    export CMAKE_SHARED_LIBRARY_SUFFIX=.dylib
+    export CMAKE_SHARED_MODULE_SUFFIX=.dylib
+    export MACOSX_RPATH=ON
+  '';
+
+postBuild = lib.optionalString (stdenv.isDarwin && lib.versionAtLeast postgresql.version "16") ''
+  shopt -s nullglob
+  for file in lib/libpgrouting-*.so; do
+    if [ -f "$file" ]; then
+      mv "$file" "''${file%.so}.dylib"
+    fi
+  done
+  shopt -u nullglob
+'';
+
   installPhase = ''
-    install -D lib/*{dylib,so}                 -t $out/lib
-    install -D sql/pgrouting--${version}.sql   -t $out/share/postgresql/extension
-    install -D sql/common/pgrouting.control    -t $out/share/postgresql/extension
+    install -D lib/*{dylib,so} -t $out/lib
+    install -D sql/pgrouting--*.sql -t $out/share/postgresql/extension
+    install -D sql/common/pgrouting.control -t $out/share/postgresql/extension
   '';
 
   meta = with lib; {
