aa5dd9a271eb4c79bf97e77e3e92d628900c281e
Sam Rose <samuel@supabase.io>
2024-10-07 16:57:53 -0400

    tests: build test and cache both versions

--- docker/nix/build_nix.sh
+++ docker/nix/build_nix.sh
@@ -6,5 +6,7 @@ if [ -d "/workspace" ]; then
     cd /workspace
 fi
 nix build .#psql_15/bin -o psql_15
+nix build .#psql_16/bin -o psql_16
 nix flake check -L 
 nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_15
+nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_16
