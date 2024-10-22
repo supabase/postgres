14282f1567b26d5c53b1e07e18e8acc327969495
Sam Rose <samuel@supabase.io>
2024-10-22 13:23:16 -0400

    fix: conflicts

--- docker/nix/build_nix.sh
+++ docker/nix/build_nix.sh
@@ -9,12 +9,6 @@ nix build .#checks.$(nix-instantiate --eval -E builtins.currentSystem | tr -d '"
 nix build .#checks.$(nix-instantiate --eval -E builtins.currentSystem | tr -d '"').psql_16 -L --no-link
 nix build .#psql_15/bin -o psql_15
 nix build .#psql_16/bin -o psql_16
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 nix flake check -L 
->>>>>>> aa5dd9a (tests: build test and cache both versions)
-=======
->>>>>>> 65fdc5d (chore: run checks individually)
 nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_15
 nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_16
