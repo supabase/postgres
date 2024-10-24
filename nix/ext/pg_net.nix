2d658fc9fe74a386b9427fd0a614820a658e0cf0
Oliver Rice <github@oliverrice.com>
2024-10-24 10:24:33 -0500

    pg_net 0.11 on 15.6 release branch (#1290)
    * bump pg_net to 0.11.0
    
    * bump image to 15.6.1.135

--- nix/ext/pg_net.nix
+++ nix/ext/pg_net.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "pg_net";
-  version = "0.10.0";
+  version = "0.11.0";
 
   buildInputs = [ curl postgresql ];
 
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
     owner = "supabase";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-R9Mzw5gvV7b2R59LTOzuOc0AI99+3ncFNzijI4mySUg=";
+    hash = "sha256-XN441jXK1q+I/LZRNwvzbSsebXHgZ8iYsslZvcPFlAs=";
   };
 
   env.NIX_CFLAGS_COMPILE = "-Wno-error";
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
   installPhase = ''
     mkdir -p $out/{lib,share/postgresql/extension}
 
-    cp *.so      $out/lib
+    cp *${postgresql.dlSuffix}      $out/lib
     cp sql/*.sql $out/share/postgresql/extension
     cp *.control $out/share/postgresql/extension
   '';
