28f75c83eb470f868726e39e610060d19ddc9466
Oliver Rice <github@oliverrice.com>
2024-11-01 10:37:43 -0500

    remove pg_partman

--- flake.nix
+++ flake.nix
@@ -151,7 +151,6 @@
           ./nix/ext/pg_hashids.nix
           ./nix/ext/pgsodium.nix
           ./nix/ext/pg_graphql.nix
-          ./nix/ext/pg_partman.nix
           ./nix/ext/pg_stat_monitor.nix
           ./nix/ext/pg_jsonschema.nix
           ./nix/ext/pgvector.nix
