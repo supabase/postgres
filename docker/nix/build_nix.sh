84d78d6c2fbbb9f9ef5ece15c1f6f58540994788
Sam Rose <samuel@supabase.io>
2024-11-22 08:52:34 -0500

    fix: skip x86 darwin for oriole nix builds

--- docker/nix/build_nix.sh
+++ docker/nix/build_nix.sh
@@ -5,25 +5,23 @@ nix --version
 if [ -d "/workspace" ]; then
     cd /workspace
 fi
-echo "Running on non-macOS arm64 and amd64, remove me when pg_net supports darwin arm64" 
-nix build .#checks.$(nix-instantiate --eval -E builtins.currentSystem | tr -d '"').psql_15 -L --no-link
-nix build .#checks.$(nix-instantiate --eval -E builtins.currentSystem | tr -d '"').psql_16 -L --no-link
-#no nix flake check on oriole yet
+
+SYSTEM=$(nix-instantiate --eval -E builtins.currentSystem | tr -d '"')
+
+nix build .#checks.$SYSTEM.psql_15 -L --no-link
+nix build .#checks.$SYSTEM.psql_16 -L --no-link
+
 nix build .#psql_15/bin -o psql_15
 nix build .#psql_16/bin -o psql_16
-nix build .#psql_orioledb-17/bin -o psql_orioledb_17
-# nix build .#postgresql_15_src -o psql_15_src
-# nix build .#postgresql_16_src -o psql_16_src
-# nix build .#postgresql_orioledb-17_src -o psql_orioledb_17_src
-#nix build .#postgresql_15_debug -o psql_15_debug
-#nix build .#postgresql_16_debug -o psql_16_debug
-#nix build .#postgresql_orioledb-17_debug -o psql_orioledb_17_debug
+
+# Skip orioledb-17 on x86_64-darwin
+if [ "$SYSTEM" != "x86_64-darwin" ]; then
+    nix build .#psql_orioledb-17/bin -o psql_orioledb_17
+fi
+
+# Copy to S3
 nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_15
 nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_16
-nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_orioledb_17
-# nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_15_src
-# nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_16_src
-# nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_orioledb_17_src
-# nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_15_debug
-# nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_16_debug
-# nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_orioledb_17_debug
+if [ "$SYSTEM" != "x86_64-darwin" ]; then
+    nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_orioledb_17
+fi
