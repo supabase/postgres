4754e2948f18efd35bb059fc5241ca551aa67f1c
zel <mxyakovenko9@gmail.com>
2025-01-01 16:00:16 +0300

    Feature, Add mysql_fdw extension

---
+++ nix/ext/mysql_fdw.nix
@@ -0,0 +1,47 @@
+{ lib, stdenv, fetchFromGitHub, postgresql, libmysqlclient }:
+
+stdenv.mkDerivation ( finalAttrs: {
+  pname = "mysql_fdw";
+  version = "REL-2_9_2";
+
+  buildInputs = [ postgresql libmysqlclient ];
+
+  src = fetchFromGitHub {
+    owner = "EnterpriseDB";
+    repo = finalAttrs.pname;
+    rev = "refs/tags/${finalAttrs.version}";
+    sha256 = "sha256-CLnSaV+pv+i/k1RlFmMVMK9hXWQkJvEim/xYghXv2cs=";
+  };
+
+  configure = ''
+    export LDFLAGS="-L${libmysqlclient}/lib/mariadb -lmysqlclient"
+  '';
+
+  makeFlags = [
+    "USE_PGXS=1"
+    "LDFLAGS+=-L${libmysqlclient}/lib/mariadb -lmysqlclient"
+    "CPPFLAGS=-I${libmysqlclient}/include/mysql"
+  ];
+
+  installPhase = ''
+    mkdir -p $out/{lib,share/postgresql/extension}
+    
+    cp *${postgresql.dlSuffix}      $out/lib
+    cp *.sql     $out/share/postgresql/extension
+    cp *.control $out/share/postgresql/extension
+  '';
+
+  postInstall = ''
+    ln -s ${libmysqlclient}/lib/mariadb/libmysqlclient.so ${postgresql.lib}/lib/libmysqlclient.so
+    ldconfig
+  '';
+
+  meta = with lib; {
+    description = "MySQL foreign data wrapper for PostgreSQL";
+    homepage    = "https://github.com/EnterpriseDB/mysql_fdw";
+    changelog   = "https://github.com/EnterpriseDB/mysql_fdw/releases/tags/${finalAttrs.version}";
+    maintainers = with maintainers; [ thelazzziest ];
+    inherit (postgresql.meta) platforms;
+    license     = licenses.postgresql;
+  };
+})
