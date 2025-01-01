9bdf29c489147c4c7175a07f550771ff7e0459c5
zel <mxyakovenko9@gmail.com>
2025-01-01 20:15:11 +0300

    Feature, Connect mysql_fdw derivative to the flake

--- flake.nix
+++ flake.nix
@@ -137,6 +137,7 @@
           ./nix/ext/wrappers/default.nix
           ./nix/ext/supautils.nix
           ./nix/ext/plv8.nix
+          ./nix/ext/mysql_fdw.nix
         ];
 
         #Where we import and build the orioledb extension, we add on our custom extensions
