d04658b478bc5a077044aeb5940904d1f37cf7a4
Sam Rose <samuel@supabase.io>
2025-01-07 14:26:07 -0500

    chore: deprecate x86_64-darwin and run check on oriole17

--- docker/nix/build_nix.sh
+++ docker/nix/build_nix.sh
@@ -10,14 +10,11 @@ SYSTEM=$(nix-instantiate --eval -E builtins.currentSystem | tr -d '"')
 
 nix build .#checks.$SYSTEM.psql_15 -L --no-link
 nix build .#checks.$SYSTEM.psql_16 -L --no-link
-
+nix build .#checks.$SYSTEM.psql_orioledb-17 -L --no-link
 nix build .#psql_15/bin -o psql_15
 nix build .#psql_16/bin -o psql_16
 
-# Skip orioledb-17 on x86_64-darwin
-if [ "$SYSTEM" != "x86_64-darwin" ]; then
-    nix build .#psql_orioledb-17/bin -o psql_orioledb_17
-fi
+nix build .#psql_orioledb-17/bin -o psql_orioledb_17
 
 # Copy to S3
 nix copy --to s3://nix-postgres-artifacts?secret-key=nix-secret-key ./psql_15
