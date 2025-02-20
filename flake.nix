1b2b4367e18d14044197a85697d7f114c3cbdf84
Sam Rose <samuel@supabase.io>
2025-02-20 12:22:19 -0500

    feature: wal-g v 3

--- flake.nix
+++ flake.nix
@@ -87,6 +87,7 @@
         sfcgal = pkgs.callPackage ./nix/ext/sfcgal/sfcgal.nix { };
         supabase-groonga = pkgs.callPackage ./nix/supabase-groonga.nix { };
         mecab-naist-jdic = pkgs.callPackage ./nix/ext/mecab-naist-jdic/default.nix { };
+        wal-g = pkgs.callPackage ./nix/wal-g.nix { };
         # Our list of PostgreSQL extensions which come from upstream Nixpkgs.
         # These are maintained upstream and can easily be used here just by
         # listing their name. Anytime the version of nixpkgs is upgraded, these
@@ -405,6 +406,7 @@
           # PostgreSQL versions.
           psql_15 = postgresVersions.psql_15;
           psql_orioledb-17 = postgresVersions.psql_orioledb-17;
+          wal-g = wal-g;
           sfcgal = sfcgal;
           pg_prove = pkgs.perlPackages.TAPParserSourceHandlerpgTAP;
           inherit postgresql_15 postgresql_orioledb-17;
