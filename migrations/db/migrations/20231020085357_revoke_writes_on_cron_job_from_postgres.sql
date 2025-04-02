aa73144c1e71eca6be971574b1ac9c7e22ae0c11
Bobbie Soedirgo <bobbie@soedirgo.dev>
2025-04-02 15:25:44 +0800

    feat(migrations): alter internal evt trig owner

--- migrations/db/migrations/20231020085357_revoke_writes_on_cron_job_from_postgres.sql
+++ migrations/db/migrations/20231020085357_revoke_writes_on_cron_job_from_postgres.sql
@@ -40,6 +40,7 @@ END;
 $$;
 
 drop event trigger if exists issue_pg_cron_access;
+alter function extensions.grant_pg_cron_access owner to supabase_admin;
 CREATE EVENT TRIGGER issue_pg_cron_access ON ddl_command_end
          WHEN TAG IN ('CREATE EXTENSION')
    EXECUTE FUNCTION extensions.grant_pg_cron_access();
