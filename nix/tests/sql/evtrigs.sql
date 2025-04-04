9886b2c3c52e0809fdb9d6663fcc304e4af16bff
steve-chavez <stevechavezast@gmail.com>
2025-04-04 16:30:53 -0500

    test: add event trigger function schema

--- nix/tests/sql/evtrigs.sql
+++ nix/tests/sql/evtrigs.sql
@@ -1,9 +1,12 @@
-SELECT
+select
   e.evtname,
-  e.evtowner::regrole AS evtowner,
-  e.evtfoid::regproc AS evtfunction,
-  p.proowner::regrole AS function_owner
-FROM pg_event_trigger e
-JOIN pg_proc p
-  ON e.evtfoid = p.oid
-WHERE p.prorettype = 'event_trigger'::regtype;
+  e.evtowner::regrole as evtowner,
+  n_func.nspname as evtfunction_schema,
+  e.evtfoid::regproc as evtfunction,
+  p.proowner::regrole as function_owner
+from pg_event_trigger e
+join pg_proc p
+  on e.evtfoid = p.oid
+join pg_namespace n_func
+  on p.pronamespace = n_func.oid
+where p.prorettype = 'event_trigger'::regtype;
