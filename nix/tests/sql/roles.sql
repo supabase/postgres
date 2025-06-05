d8733a3e94889ce598123e70dc643b2b1c5c0368
Paul Cioanca <paul.cioanca@supabase.io>
2025-06-05 20:32:47 +0300

    chore: update snapshots; multiversion boogaloo

--- nix/tests/sql/roles.sql
+++ nix/tests/sql/roles.sql
@@ -28,22 +28,6 @@ from pg_roles r
 where rolname not in ('pg_create_subscription', 'pg_maintain', 'pg_use_reserved_connections')
 order by rolname;
 
--- all role memberships
-select
-    r.rolname as member,
-    g.rolname as "member_of (can become)",
-    m.admin_option
-from
-    pg_roles r
-left join
-    pg_auth_members m on r.oid = m.member
-left join
-    pg_roles g on m.roleid = g.oid
-where r.rolname not in ('pg_create_subscription', 'pg_maintain', 'pg_use_reserved_connections')
-and g.rolname not in ('pg_create_subscription', 'pg_maintain', 'pg_use_reserved_connections')
-order by
-    r.rolname, g.rolname;
-
 -- Check all privileges of the roles on the schemas
 select schema_name, privilege_type, grantee, default_for
 from (
