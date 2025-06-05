2ce63b08347a879fbd1ad1bf60c0718e45c3abd2
Bobbie Soedirgo <bobbie@soedirgo.dev>
2025-06-05 19:38:25 +0200

    fix: add more roles

--- migrations/db/migrations/20250605172253_grant_with_admin_to_postgres_16_and_above.sql
+++ migrations/db/migrations/20250605172253_grant_with_admin_to_postgres_16_and_above.sql
@@ -6,7 +6,7 @@ BEGIN
   SELECT current_setting('server_version_num')::INT / 10000 INTO major_version;
 
   IF major_version >= 16 THEN
-    GRANT anon, authenticated, service_role TO postgres WITH ADMIN OPTION;
+    GRANT anon, authenticated, service_role, authenticator, pg_monitor, pg_read_all_data, pg_signal_backend TO postgres WITH ADMIN OPTION;
   END IF;
 END $$;
 
--- nix/tests/sql/z_17_roles.sql
+++ nix/tests/sql/z_17_roles.sql
@@ -32,8 +32,6 @@ left join
     pg_auth_members m on r.oid = m.member
 left join
     pg_roles g on m.roleid = g.oid
-where r.rolname in ('pg_create_subscription', 'pg_maintain', 'pg_use_reserved_connections')
-or g.rolname in ('pg_create_subscription', 'pg_maintain', 'pg_use_reserved_connections')
 order by
     r.rolname, g.rolname;
 
