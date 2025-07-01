d9a6d8945797fea75af95237b2c9e86cf9847b84
Sam Rose <samuel@supabase.io>
2025-06-30 20:13:02 -0400

    fix: netplan apply after upgrade

--- ansible/files/admin_api_scripts/pg_upgrade_scripts/complete.sh
+++ ansible/files/admin_api_scripts/pg_upgrade_scripts/complete.sh
@@ -333,6 +333,9 @@ function complete_pg_upgrade {
         echo "5.1. Restarting gotrue and postgrest"
         retry 3 service gotrue restart
         retry 3 service postgrest restart
+        
+        echo "5.2. Applying netplan configuration to fix network issues"
+        retry 3 sudo netplan apply
     else
         retry 3 CI_stop_postgres || true
         retry 3 CI_start_postgres
