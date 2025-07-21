7176b6d629c779d41cc87407171eef1e9a048f61
Jean-François Roche <jfroche@pyxel.be>
2025-07-21 23:58:38 +0200

    feat(postgresql): simplify systemdSupport default value
    Backport upstream commit https://github.com/NixOS/nixpkgs/commit/a7fdf2256ae643876035c863dc9bbd3fd95101da

--- nix/postgresql/generic.nix
+++ nix/postgresql/generic.nix
@@ -36,9 +36,7 @@ let
       docbook_xsl_ns,
       libxslt,
 
-      # This is important to obtain a version of `libpq` that does not depend on systemd.
-      systemdSupport ?
-        lib.meta.availableOn stdenv.hostPlatform systemdLibs && !stdenv.hostPlatform.isStatic,
+      systemdSupport ? lib.meta.availableOn stdenv.hostPlatform systemdLibs,
       enableSystemd ? null,
       gssSupport ? with stdenv.hostPlatform; !isWindows && !isStatic,
 
