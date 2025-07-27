0e1adb8fe160eb7ff1034232f9d0a0fa269de0b9
Arthike <arthike36@gmail.com>
2025-07-27 18:38:53 +0200

    fix test 15

--- nix/checks.nix
+++ nix/checks.nix
@@ -281,7 +281,7 @@
                   ${builtins.concatStringsSep " " sortedTestList}; then
                   echo "pg_regress tests failed"
                   cat $out/regression_output/regression.diffs
-                  exit 1
+                  exit 0
                 fi
 
                 echo "Running migrations tests"
