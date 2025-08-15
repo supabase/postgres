9655d14de2bad7d16eeded224ad204792ccdb727
Sam Rose <samuel@supabase.io>
2025-08-15 15:19:26 -0400

    fix: new minor version introduces --restrict-key which we will set with arbitrary value ONLY IN TESTING context No one should EVER use the schemas in our repo for restoration of production databases!

--- migrations/schema-17.sql
+++ migrations/schema-17.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict wJhXZ3zzSxdOnzT40iWgcbF8W1OVaekHNId1sH6S7U0SApyR73uF8dYaLqibgGY
+\restrict SupabaseTestDumpKey123
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -1000,5 +1000,5 @@ CREATE EVENT TRIGGER pgrst_drop_watch ON sql_drop
 -- PostgreSQL database dump complete
 --
 
-\unrestrict wJhXZ3zzSxdOnzT40iWgcbF8W1OVaekHNId1sH6S7U0SApyR73uF8dYaLqibgGY
+\unrestrict SupabaseTestDumpKey123
 
