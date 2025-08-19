cb61258dcaf95e4e7582c294f2c0d2a29559ef08
Sam Rose <samuel@supabase.io>
2025-08-19 09:34:24 -0400

    fix: fmt

--- nix/ext/pg_net.nix
+++ nix/ext/pg_net.nix
@@ -120,8 +120,7 @@ let
       buildInputs = [
         curl
         postgresql
-      ]
-      ++ lib.optional (version == "0.6") libuv;
+      ] ++ lib.optional (version == "0.6") libuv;
 
       src = fetchFromGitHub {
         owner = "supabase";
