68f882abedb7a2fae714d5398e91eef8ecb78799
samrose <samuel.rose@gmail.com>
2025-09-10 15:34:54 -0400

    tests: try to get passing tests

--- nix/ext/tests/default.nix
+++ nix/ext/tests/default.nix
@@ -28,6 +28,9 @@ let
               inherit (postgresql) version psqlSchema;
               lib = pkg;
               withPackages = _: pkg;
+              withJIT = pkg;
+              withoutJIT = pkg;
+              installedExtensions = [ (installedExtension majorVersion) ];
             };
             nativeBuildInputs = [ pkgs.makeWrapper ];
             pathsToLink = [
