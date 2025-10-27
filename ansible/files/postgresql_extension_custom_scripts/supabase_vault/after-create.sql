e781ea96da854605c266189ad3a9f20b29be3965
Bobbie Soedirgo <bobbie@soedirgo.dev>
2025-10-27 15:08:11 -0400

    fix: update ext custom scripts to follow changes in Salt

--- ansible/files/postgresql_extension_custom_scripts/supabase_vault/after-create.sql
+++ ansible/files/postgresql_extension_custom_scripts/supabase_vault/after-create.sql
@@ -1,8 +1,17 @@
-grant usage on schema vault to postgres with grant option;
-grant select, delete, truncate, references on vault.secrets, vault.decrypted_secrets to postgres with grant option;
-grant execute on function vault.create_secret, vault.update_secret, vault._crypto_aead_det_decrypt to postgres with grant option;
+do $$
+declare
+  extversion text := (select extversion from pg_extension where extname = 'supabase_vault');
+begin
+  set local search_path = '';
 
--- service_role used to be able to manage secrets in Vault <=0.2.8 because it had privileges to pgsodium functions
-grant usage on schema vault to service_role;
-grant select, delete on vault.secrets, vault.decrypted_secrets to service_role;
-grant execute on function vault.create_secret, vault.update_secret, vault._crypto_aead_det_decrypt to service_role;
+  if extversion != '0.2.8' then
+    grant usage on schema vault to postgres with grant option;
+    grant select, delete, truncate, references on vault.secrets, vault.decrypted_secrets to postgres with grant option;
+    grant execute on function vault.create_secret, vault.update_secret, vault._crypto_aead_det_decrypt to postgres with grant option;
+
+    -- service_role used to be able to manage secrets in Vault <=0.2.8 because it had privileges to pgsodium functions
+    grant usage on schema vault to service_role;
+    grant select, delete on vault.secrets, vault.decrypted_secrets to service_role;
+    grant execute on function vault.create_secret, vault.update_secret, vault._crypto_aead_det_decrypt to service_role;
+  end if;
+end $$;
