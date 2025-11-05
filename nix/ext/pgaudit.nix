110f443c453f98e22f3f7207a86ad87f7a43ecab
Sam Rose <samuel@supabase.io>
2025-11-05 11:31:43 -0500

    fix: just use the the name

--- nix/ext/pgaudit.nix
+++ nix/ext/pgaudit.nix
@@ -188,7 +188,7 @@ buildEnv {
     version =
       "multi-" + lib.concatStringsSep "-" (map (v: lib.replaceStrings [ "." ] [ "-" ] v) versions);
     defaultSettings = {
-      shared_preload_libraries = [ (lib.removeSuffix "-all" "${pname}-all") ];
+      shared_preload_libraries = [ "pgaudit" ];
     };
   };
 }
