e4801532b3621985a795351552c317823f3238cc
Artur Zakirov <zaartur@gmail.com>
2025-11-05 21:16:10 +0100

    Update migrations/db/migrations/20251105172723_grant_pg_reload_conf_to_postgres.sql
    Co-authored-by: Bobbie Soedirgo <31685197+soedirgo@users.noreply.github.com>

--- migrations/db/migrations/20251105172723_grant_pg_reload_conf_to_postgres.sql
+++ migrations/db/migrations/20251105172723_grant_pg_reload_conf_to_postgres.sql
@@ -1,5 +1,5 @@
 -- migrate:up
-grant execute on function pg_catalog.pg_reload_conf() to postgres;
+grant execute on function pg_catalog.pg_reload_conf() to postgres with grant option;
 
 -- migrate:down
 
