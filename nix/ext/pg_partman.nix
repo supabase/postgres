b7311ee7f8ba6668c30d29f7e5abaad5a7ca0d06
Chris Gwilliams <517923+encima@users.noreply.github.com>
2025-11-10 11:57:59 +0200

    update partman to 5.3.1

--- nix/ext/pg_partman.nix
+++ nix/ext/pg_partman.nix
@@ -7,7 +7,7 @@
 
 stdenv.mkDerivation rec {
   pname = "pg_partman";
-  version = "5.2.4";
+  version = "5.3.1";
 
   buildInputs = [ postgresql ];
 
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
     owner = "pgpartman";
     repo = "${pname}";
     rev = "refs/tags/v${version}";
-    hash = "sha256-i/o+JZEXnJRO17kfdTw87aca28+I8pvuFZsPMA/kf+w=";
+    hash = "sha256-LK+z/0puUuNLz2ah/SDaxxyLS/On/cmCBhKNwTLa7Pk=";
   };
 
   installPhase = ''
