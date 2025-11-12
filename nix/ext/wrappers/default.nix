2743e07978bce356b66655e42cbdf853b44d78ac
Yvan Sraka <yvan@sraka.xyz>
2025-11-12 15:44:05 +0100

    feat(wrappers): add versions 0.1.14 and 0.1.15

--- nix/ext/wrappers/default.nix
+++ nix/ext/wrappers/default.nix
@@ -188,8 +188,6 @@ let
     );
   # All versions that were previously packaged (historical list)
   allPreviouslyPackagedVersions = [
-    "0.1.15"
-    "0.1.14"
     "0.1.12"
     "0.1.11"
     "0.1.10"
