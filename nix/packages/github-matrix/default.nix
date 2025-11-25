53120db0d05d96e840f0567bcaeefcada2adb7e8
Jean-François Roche <jfroche@pyxel.be>
2025-11-25 21:47:43 +0100

    feat(github-matrix): integrate github-action-utils for better error visibility
    Integrates github-action-utils library to improve error and warning
    visibility in GitHub Actions UI through workflow command annotations.

--- nix/packages/github-matrix/default.nix
+++ nix/packages/github-matrix/default.nix
@@ -5,6 +5,25 @@
 }:
 let
   pname = "github-matrix";
+
+  github-action-utils = python3Packages.buildPythonPackage rec {
+    pname = "github-action-utils";
+    version = "1.1.0";
+    pyproject = true;
+
+    src = python3Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "0q9xrb4jcvbn6954lvpn85gva1yc885ykdqb2q2410cxp280v94a";
+    };
+
+    build-system = with python3Packages; [ setuptools ];
+
+    meta = with lib; {
+      description = "Collection of Python functions for GitHub Action Workflow Commands";
+      homepage = "https://github.com/saadmk11/github-action-utils";
+      license = licenses.mit;
+    };
+  };
 in
 
 python3Packages.buildPythonApplication {
@@ -13,6 +32,11 @@ python3Packages.buildPythonApplication {
   pyproject = false;
 
   src = ./.;
+
+  propagatedBuildInputs = [
+    github-action-utils
+    python3Packages.result
+  ];
 
   makeWrapperArgs = [ "--suffix PATH : ${lib.makeBinPath [ nix-eval-jobs ]}" ];
 
