bc5be80bf1d942f988019600c00124ccd96adb4e
Etienne Stalmans <etienne@supabase.io>
2025-12-12 13:37:56 +0100

    fix: go version

--- nix/packages/gatekeeper.nix
+++ nix/packages/gatekeeper.nix
@@ -1,20 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 let
-  upstream-src = pkgs.fetchFromGitHub {
-    owner = "supabase";
-    repo = "jit-db-gatekeeper";
-    rev = "v1.0.0";
-    sha256 = "sha256-C4RPyzpItJrM/FxINpEIKvkYdbfaFXK0hBJe17PpejM=";
-  };
 
-  upstream-gatekeeper = pkgs.buildGoModule {
+  go124 = inputs.nixpkgs-go124.legacyPackages.${pkgs.system}.go_1_24;
+  buildGoModule124 = pkgs.buildGoModule.override { go = go124; };
+
+  upstream-gatekeeper = buildGoModule124 {
     pname = "jit-db-gatekeeper";
     version = "1.0.0";
-
-    src = upstream-src;
-
-    # Get vendorHash by setting to null first, building, and using error message
+    src = pkgs.fetchFromGitHub {
+      owner = "supabase";
+      repo = "jit-db-gatekeeper";
+      rev = "v1.0.0";
+      sha256 = "sha256-hdy2uaq1igNouCs6GHhRYQADeyWnXZ4+W+4YiyEUtZw=";
+    };
     vendorHash = null;
+
+    buildInputs = [ pkgs.pam ];
 
     # Environment variables - choose ONE approach
     CGO_ENABLED = "1";
@@ -37,6 +38,6 @@ pkgs.stdenv.mkDerivation {
 
   installPhase = ''
     mkdir -p $out/lib/security/
-    cp ${upstream-gatekeeper}/pam_jit_pg.so $out/lib/security/
+    cp ${upstream-gatekeeper}/bin/jit-db-gatekeeper $out/lib/security/pam_jit_pg.so
   '';
 }
