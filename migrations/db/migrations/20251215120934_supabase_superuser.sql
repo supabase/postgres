d48821273f6b067a1139d338f4e9d7b6412a5976
Bobbie Soedirgo <bobbie@soedirgo.dev>
2026-02-09 12:45:26 +0800

    feat/rename-supabase-privileged-role

--- migrations/db/migrations/20251215120934_supabase_superuser.sql
+++ migrations/db/migrations/20251215120934_supabase_superuser.sql
@@ -1,9 +1,9 @@
 -- migrate:up
 do $$
 begin
-  if not exists (select from pg_roles where rolname = 'supabase_superuser') then
-    create role supabase_superuser;
-    grant supabase_superuser to postgres, supabase_etl_admin;
+  if not exists (select from pg_roles where rolname = 'supabase_privileged_role') then
+    create role supabase_privileged_role;
+    grant supabase_privileged_role to postgres, supabase_etl_admin;
   end if;
 end $$;
 
