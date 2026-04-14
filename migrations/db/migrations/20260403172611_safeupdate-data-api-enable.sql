4f0a4ee578d0b385c6c4be8842e705c5ef239f59
Chris Gwilliams <517923+encima@users.noreply.github.com>
2026-04-14 19:25:22 +0300

    remove session preload libraries from anon and authenticated

--- migrations/db/migrations/20260403172611_safeupdate-data-api-enable.sql
+++ migrations/db/migrations/20260403172611_safeupdate-data-api-enable.sql
@@ -1,7 +1,5 @@
 -- migrate:up
-ALTER ROLE anon SET session_preload_libraries = '$libdir/plugins/safeupdate';
 ALTER ROLE authenticator SET session_preload_libraries = 'supautils, $libdir/plugins/safeupdate';
-ALTER ROLE authenticated SET session_preload_libraries = '$libdir/plugins/safeupdate';
 ALTER ROLE postgres SET local_preload_libraries = '$libdir/plugins/safeupdate';
 
 ALTER ROLE anon SET safeupdate.enabled = 1;
