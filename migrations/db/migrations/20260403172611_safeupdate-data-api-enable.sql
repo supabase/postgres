04f4c152973665d83ddac3521f0062135a656810
Chris Gwilliams <517923+encima@users.noreply.github.com>
2026-04-15 08:47:34 +0300

    restrict migration changes to postgres role

--- migrations/db/migrations/20260403172611_safeupdate-data-api-enable.sql
+++ migrations/db/migrations/20260403172611_safeupdate-data-api-enable.sql
@@ -2,9 +2,6 @@
 ALTER ROLE authenticator SET session_preload_libraries = 'supautils, $libdir/plugins/safeupdate';
 ALTER ROLE postgres SET local_preload_libraries = '$libdir/plugins/safeupdate';
 
-ALTER ROLE anon SET safeupdate.enabled = 1;
-ALTER ROLE authenticator SET safeupdate.enabled = 1;
-ALTER ROLE authenticated SET safeupdate.enabled = 1;
 ALTER ROLE postgres SET safeupdate.enabled = 0;
 
 
