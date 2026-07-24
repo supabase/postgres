0aca850eeb929675ad4bbc4063b0aaceac570a25
Raminder Singh <romi_ssk@yahoo.co.in>
2026-07-24 11:09:42 +0530

    chore: bump supautils to 3.4.0

--- nix/ext/supautils.nix
+++ nix/ext/supautils.nix
@@ -8,7 +8,7 @@
 stdenv.mkDerivation rec {
   pname = "supautils";
   name = pname;
-  version = "3.3.0";
+  version = "3.4.0";
 
   buildInputs = [ postgresql ];
 
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
     owner = "supabase";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-yGKl5vUrfvFf4eQaEbTOURMbQdRPFhdmKXgz1WKZwCU=";
+    hash = "sha256-O2zVVVf2OFTCc4BYHuGJ67odU0TwMnTJQuz9uk+a4/0=";
   };
 
   installPhase = ''
