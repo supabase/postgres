d136628816d046f007938ac70e37ab95d4d867f1
Manuel Mendez <git@i.m.mmlb.dev>
2026-07-30 16:01:54 -0400

    packer: Remove dead code from cleanup scripts
    No yum handling necessary and commented out code should be deleted.

--- ebssurrogate/scripts/cleanup-qemu.sh
+++ ebssurrogate/scripts/cleanup-qemu.sh
@@ -14,63 +14,43 @@ if [[ ! -d /tmp ]]; then
 fi
 chmod 1777 /tmp
 
-if [ -n "$(command -v yum)" ]; then
-	yum update -y
-	yum clean all
-elif [ -n "$(command -v apt-get)" ]; then
-	# Cleanup more packages
-	apt-get -y remove --purge \
-		automake \
-		autoconf \
-		autotools-dev \
-		cmake-data \
-		cpp-9 \
-		cpp-10 \
-		gcc-9 \
-		gcc-10 \
-		git \
-		git-man \
-		ansible \
-		libicu-dev \
-		libcgal-dev \
-		libgcc-9-dev \
-		ansible \
-		snapd
+# Cleanup more packages
+apt-get -y remove --purge \
+	automake \
+	autoconf \
+	autotools-dev \
+	cmake-data \
+	cpp-9 \
+	cpp-10 \
+	gcc-9 \
+	gcc-10 \
+	git \
+	git-man \
+	ansible \
+	libicu-dev \
+	libcgal-dev \
+	libgcc-9-dev \
+	ansible \
+	snapd
 
-	if [[ $(uname -m) == aarch64 ]]; then
-		apt-get -y remove --purge libgcc-8-dev
-	fi
-
-	# add-apt-repository --yes --remove ppa:ansible/ansible
-
-	apt-mark manual libevent-2.1-7t64
-
-	apt-get remove -y --purge ansible-core apport appstream bash-completion bcache-tools bind9-dnsutils bind9-host bind9-libs bolt btrfs-progs byobu command-not-found console-setup distro-info eject fonts-ubuntu-console friendly-recovery ftp fwupd gawk gdisk keyboard-configuration libvolume-key1 libssl-dev lvm2 lxd-agent-loader man-db mdadm modemmanager mtd-utils nano netcat-openbsd nfs-common ntfs-3g parted pastebinit screen strace thin-provisioning-tools tmux usb-modeswitch vim vim-runtime wget whiptail xfsprogs
-
-	apt remove -y --purge libc6-dev linux-libc-dev libevent-dev libpcre3-dev libsystemd-dev packagekit multipath-tools unattended-upgrades plymouth gnupg open-vm-tools xauth lxd-installer publicsuffix libclang-cpp18 python3-twisted python-babel-localedata libicu74 python3-pygments fonts-dejavu* python3-botocore
-
-	apt-get remove -y --purge linux-headers*
-
-	# remove old kernels
-	# CURRENT_KERNEL="$(uname -r | sed 's/-generic//')"
-	# INSTALLED_KERNELS=$(dpkg -l | awk '{print $2}' | grep -Eo 'linux-(image|headers|modules|tools)-[0-9]+' | sed -E 's/linux-(image|modules|tools)-//' | sort -Vu)
-	# REMOVE_KERNELS=$(echo "$INSTALLED_KERNELS" | grep -v -e "$CURRENT_KERNEL")
-	# for VER in $REMOVE_KERNELS; do
-	#     for PREFIX in linux-image linux-modules linux-tools; do
-	#         for PKG in $(dpkg -l | awk '{print $2}' | grep "^$PREFIX-$VER"); do
-	#             apt-get purge -y "$PKG"
-	#         done
-	#     done
-	# done
-	# update-grub
-
-	apt-get -y autoremove
-	apt-get -y autoclean
-
-	apt-get -y update
-	apt-get -y upgrade
-
+if [[ $(uname -m) == aarch64 ]]; then
+	apt-get -y remove --purge libgcc-8-dev
 fi
+
+# add-apt-repository --yes --remove ppa:ansible/ansible
+
+apt-mark manual libevent-2.1-7t64
+
+apt-get remove -y --purge ansible-core apport appstream bash-completion bcache-tools bind9-dnsutils bind9-host bind9-libs bolt btrfs-progs byobu command-not-found console-setup distro-info eject fonts-ubuntu-console friendly-recovery ftp fwupd gawk gdisk keyboard-configuration libvolume-key1 libssl-dev lvm2 lxd-agent-loader man-db mdadm modemmanager mtd-utils nano netcat-openbsd nfs-common ntfs-3g parted pastebinit screen strace thin-provisioning-tools tmux usb-modeswitch vim vim-runtime wget whiptail xfsprogs
+
+apt remove -y --purge libc6-dev linux-libc-dev libevent-dev libpcre3-dev libsystemd-dev packagekit multipath-tools unattended-upgrades plymouth gnupg open-vm-tools xauth lxd-installer publicsuffix libclang-cpp18 python3-twisted python-babel-localedata libicu74 python3-pygments fonts-dejavu* python3-botocore
+
+apt-get remove -y --purge linux-headers*
+
+apt-get -y autoremove
+apt-get -y autoclean
+apt-get -y update
+apt-get -y upgrade
 
 systemctl set-default multi-user.target
 systemctl disable getty@tty1.service
