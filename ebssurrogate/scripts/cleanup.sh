4a920531ac387cd78fef3f15ec8886d5b5c3d8c5
Manuel Mendez <git@i.m.mmlb.dev>
2026-07-30 16:01:54 -0400

    packer: Remove dead code from cleanup scripts
    No yum handling necessary and commented out code should be deleted.

--- ebssurrogate/scripts/cleanup.sh
+++ ebssurrogate/scripts/cleanup.sh
@@ -14,35 +14,31 @@ if [[ ! -d /tmp ]]; then
 fi
 chmod 1777 /tmp
 
-if [ -n "$(command -v yum)" ]; then
-	yum update -y
-	yum clean all
-elif [ -n "$(command -v apt-get)" ]; then
-	# Cleanup more packages
-	apt-get -y remove --purge \
-		automake \
-		autoconf \
-		autotools-dev \
-		cmake-data \
-		cpp-9 \
-		cpp-10 \
-		gcc-9 \
-		gcc-10 \
-		git \
-		git-man \
-		ansible \
-		libicu-dev \
-		libcgal-dev \
-		libgcc-9-dev \
-		ansible
+# Cleanup more packages
+apt-get -y remove --purge \
+	automake \
+	autoconf \
+	autotools-dev \
+	cmake-data \
+	cpp-9 \
+	cpp-10 \
+	gcc-9 \
+	gcc-10 \
+	git \
+	git-man \
+	ansible \
+	libicu-dev \
+	libcgal-dev \
+	libgcc-9-dev \
+	ansible
 
-	# add-apt-repository --yes --remove ppa:ansible/ansible
+# add-apt-repository --yes --remove ppa:ansible/ansible
 
-	apt-get -y update
-	apt-get -y upgrade
-	apt-get -y autoremove
-	apt-get -y autoclean
-fi
+apt-get -y update
+apt-get -y upgrade
+apt-get -y autoremove
+apt-get -y autoclean
+
 rm -rf /tmp/* /var/tmp/*
 history -c
 cat /dev/null >/root/.bash_history
