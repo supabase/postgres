99472ace704a4f64d8de65a3a3e61ee85057302a
Márton Boros <martonboros@gmail.com>
2026-09-07 22:08:34 +0300

    site-env-update: skip realize+set when profile already current

--- nix/packages/site-env.nix
+++ nix/packages/site-env.nix
@@ -49,6 +49,7 @@
             "$catalog" --region ap-southeast-1
 
           path="$(jq -er --arg s "$system" '.[$s]' "$catalog")"
+          [ "$(readlink -f /nix/var/nix/profiles/site)" = "$path" ] && exit 0
           nix-store -r --option stalled-download-timeout 120 "$path" >/dev/null
           nix-env --profile /nix/var/nix/profiles/site --set "$path"
         '';
