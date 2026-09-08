3d6869aa9f73464215b12c82a91e6aaec55c520f
Márton Boros <martonboros@gmail.com>
2026-09-08 04:36:33 +0300

    supautils: match prod debug symbols and glibc symbol versions

--- nix/ext/supautils.nix
+++ nix/ext/supautils.nix
@@ -12,12 +12,16 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ postgresql ];
 
+  dontStrip = true;
+
   src = fetchFromGitHub {
     owner = "supabase";
     repo = pname;
     rev = "refs/tags/v${version}";
     hash = "sha256-O2zVVVf2OFTCc4BYHuGJ67odU0TwMnTJQuz9uk+a4/0=";
   };
+
+  patches = [ ./patches/supautils-strtol-glibc-compat.patch ];
 
   installPhase = ''
     mkdir -p $out/lib
